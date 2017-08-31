9196206ffb614d8cc8188896ef811f58234d69d1
pablodiegoss <pablodiegoss@hotmail.com>
2017-08-31 15:02:49 -0300

    Setup Pep8

--- quero_cultura/settings.py
+++ quero_cultura/settings.py
@@ -86,7 +86,8 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimila\
+        rityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
