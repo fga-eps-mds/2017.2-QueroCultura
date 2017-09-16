e0c3074485628eb1cfdd8d80ed1ca91e176a188a
cjjcastro <cjj.castro3@gmail.com>
2017-09-16 18:11:38 -0300

    Create map

---
+++ quero_cultura/static/js/map.js
@@ -0,0 +1,10 @@
+var mymap = L.map('mapid').setView([-15.573, -54.1], 4.5);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2pqY2FzdHJvIiwiYSI6ImNqN21vYXpiMDFib3UzMnQ2OG1uM205NWEifQ.8sFAUtZu22lf_o3kmEVlMg', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.light',
+    accessToken: 'your.mapbox.access.token'
+    }).addTo(mymap);
+
+    console.log("asdf");
