58f5c1b159004ba28864c7f1ad8eaff883e5205e
Caio Oliveira <caiooliv97@gmail.com>
2017-09-16 16:53:18 -0300

    Update models and views

--- quero_cultura/models.py
+++ quero_cultura/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
 class MapPoint(models.Model):
-    typePoint = models.CharField(max_lenght=30)
+    typePoint = models.CharField(max_length=30)
     idPoint = models.IntegerField()
-    namePoint = models.CharField(max_lenght=100)
-    latitudePoint = models.CharField(max_lenght=50)
-    longitudePoint = models.CharField(max_lenght=50)
+    namePoint = models.CharField(max_length=100)
+    latitudePoint = models.CharField(max_length=50)
+    longitudePoint = models.CharField(max_length=50)
+    shortDescription = models.TextField()
+
--- quero_cultura/views.py
+++ quero_cultura/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-
+from .models import MapPoint	
 
 def index(request):
+
 	
-	return render(request, 'quero_cultura/index.html', {})
+
+	typePoint  = 'evento'
+	idPoint = 1
+	namePoint = 'Festa'
+	latitudePoint = '-45.792'
+	longitudePoint = '-17.320'
+
+	mapPoint = MapPoint(typePoint = typePoint, idPoint = idPoint, namePoint = namePoint, latitudePoint = latitudePoint, longitudePoint = longitudePoint)
+
+	
+	return render(request, 'quero_cultura/index.html', {'mapPoint' : mapPoint})
