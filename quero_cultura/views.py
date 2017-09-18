3c2b2261033d417711b97857f07cc86dc51fd1eb
cjjcastro <cjj.castro3@gmail.com>
2017-09-17 21:30:21 -0300

    Update responsive map

--- quero_cultura/views.py
+++ quero_cultura/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from .models import MapPoint	
+from .models import MapPoint
 
 def index(request):
- 
+
 
 
 	typePoint  = 'evento'
 	idPoint = 1
 	namePoint = 'Festa'
-	latitudePoint = '-16.006930'
-	longitudePoint = '-48.085224'
+	latitudePoint = '-16.021958'
+	longitudePoint = '-48.064078'
 
 	mapPoint = MapPoint(typePoint = typePoint, idPoint = idPoint, namePoint = namePoint, latitudePoint = latitudePoint, longitudePoint = longitudePoint)
 	mapPoint2 = MapPoint(typePoint = typePoint, idPoint = idPoint, namePoint = namePoint, latitudePoint = '-17', longitudePoint = '-54.002')
@@ -18,5 +18,5 @@ def index(request):
 	mapPoints.append(mapPoint2)
 
 
-	
+
 	return render(request, 'quero_cultura/index.html', {'mapPoints' : mapPoints})
