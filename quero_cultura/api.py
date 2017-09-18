7f543c3b0ff2d6d91cda976b794c0a3778d24f79
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2017-09-18 04:07:19 -0300

    Creation of the MapPointsApi class and termination of UC02 for the map of real-time updates.

--- quero_cultura/api.py
+++ quero_cultura/api.py
@@ -2,40 +2,32 @@ import requests, json
 from datetime import datetime
 from datetime import timedelta
 
-get_time = datetime.now() - timedelta(days=2)
+class MapPointsApi(object):
+    def __init__(self):
+        self._get_time = datetime.now() - timedelta(hours=18)
+        self._url = 'http://mapas.cultura.gov.br/api/agent/find/'
+        self._filters = {'@select' : 'id, name, location, createTimestamp', 'createTimestamp' : "GT("+self._get_time.__str__()+")"}
+        self._response = requests.get(self._url, self._filters)
+        self._data = json.loads(self._response.text)
 
-url = 'http://mapas.cultura.gov.br/api/agent/find/'
-filters = {'@select' : 'id, name, location, createTimestamp', 'createTimestamp' : "GT("+get_time.__str__()+")"}
+    @property
+    def response(self):
+        return self._response
 
-response = requests.get(url, filters)
+    @property
+    def data(self):
+        return self._data
 
-print(response.status_code)
+#Código abaixo serve para testar o funcionamento da classe a cima! se quiser testar basta descomentar o trecho de código.
 
-if response.status_code == 200:
-    data = json.loads(response.text)
-    print(len(data))
-
-    for i in data:
-        print(i["id"], i["name"], i["createTimestamp"]['date'])
 '''
-get_time = datetime.now()
+pontos = MapPointsApi()
 
-print(get_time)
+print(pontos.response.status_code)
 
-print(get_time.date())
-print(get_time.time())
+if pontos.response.status_code == 200:
+    print(len(pontos.data))
 
-get_time = get_time - timedelta(days=2)
-
-print(get_time)
-
-print(get_time.date())
-print(get_time.time())
-
-print(get_time.year, type(get_time.year))
-print(get_time.month, type(get_time.month))
-print(get_time.day, type(get_time.day))
-print(get_time.hour, type(get_time.hour))
-print(get_time.minute, type(get_time.minute))
-print(get_time.second, type(get_time.second))
+    for i in pontos.data:
+        print(i["id"], i["name"], i["createTimestamp"]['date'])
 '''
