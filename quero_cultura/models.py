07e2f8b651915316d6b221e7dbf7f62cec419172
Lucas Siqueira <lucassiqzro@gmail.com>
2017-09-23 17:32:50 -0300

    Create class Percent thematic museums for state

--- quero_cultura/models.py
+++ quero_cultura/models.py
@@ -51,6 +51,27 @@ class PercentThematicsMuseumsForState:
     @thmeaticsMuseumsForState.setter
     def thmeaticsMuseumsForState(self,number):
         self._thematicsMuseumsForState = number
+
+    @property
+    def totalMuseumsForState(self):
+        return self._totalMuseumsForState
+
+    @totalMuseumsForState.setter
+    def totalMuseumsForState(self,number):
+        self._totalMuseumsForState = number
+
+class PercentTypeMuseumsForState:
+    _typeMuseumsForState = MapField(required = True)
+    _totalMuseumsForState = MapField(required = True)
+
+    @property
+    def typeMuseumsForState(self):
+        return self._typeMuseumsForState
+
+    @typeMuseumsForState.setter
+    def typeMuseumsForState(self,number):
+        self._typeMuseumsForState = number
+
     @property
     def totalMuseumsForState(self):
         return self._totalMuseumsForState
@@ -60,7 +81,14 @@ class PercentThematicsMuseumsForState:
         self._totalMuseumsForState = number
 
 
-''' idPoint = IntField(required = True)
+class PercentPublicOrPrivateMuseums:
+    _totalPublicMuseums = IntField(required = True)
+    _totalPrivateMuseums = IntField(required = True)
+    _totalMuseums = IntField(required = True)
+
+
+'''
+    idPoint = IntField(required = True)
     namePoint = StringField(max_length=100)
     latitudePoint = StringField(max_length=50)
     longitudePoint = StringField(max_length=50)
