8c16ad986a4ecad8ef3e209bd0e41a9a3bd1eef6
Hugo Carvalho <hugo-nc@hotmail.com>
2017-09-23 10:17:02 -0700

    Fix envoriment settings

--- quero_cultura/settings.py
+++ quero_cultura/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from mongoengine import *
+from mongoengine import connetin
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
