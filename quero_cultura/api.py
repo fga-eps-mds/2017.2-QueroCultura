8ae78cc35a2d0f97d023eb2ab382e74afdbfd4a1
Caio Oliveira <caiooliv97@gmail.com>
2017-09-23 18:05:35 -0300

    Fix conflicts

--- quero_cultura/api.py
+++ quero_cultura/api.py
@@ -4,19 +4,13 @@ from datetime import timedelta
 
 class MapPointsApi(object):
     def __init__(self):
-<<<<<<< HEAD
-        self._get_time = datetime.now() - timedelta(hours=23)
-        self._url = 'http://mapas.cultura.gov.br/api/space/find/'
-        self._filters = {'@select' : 'id, name, location, shortDescription', '@or' : 1, 'createTimestamp' : "GT("+self._get_time.__str__()+")", 'updateTimestamp' : "GT("+self._get_time.__str__()+")"}
-=======
         self._get_time = datetime.now() - timedelta(days=23)
         self._url = 'http://mapas.cultura.gov.br/api/space/find/'
-        self._filters = { 
-        '@select' : 'id, name, location, shortDescription', 
+        self._filters = {
+        '@select' : 'id, name, location, shortDescription',
         '@or' : 1,
         'createTimestamp' : "GT("+self._get_time.__str__()+")",
         'updateTimestamp' : "GT("+self._get_time.__str__()+")"}
->>>>>>> 36cc34c78245c774bdbd7bf9206c7c7e22a194d1
         self._response = requests.get(self._url, self._filters)
         self._data = json.loads(self._response.text)
 
