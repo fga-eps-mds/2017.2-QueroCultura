cc48ae8d91127c01bc5823eb23f387cb47494450
Igor Guimarães <velvet.guimaraes@gmail.com>
2017-09-25 04:20:13 -0300

    Created API connection with the librarys

--- libery_indicators/api_connections.py
+++ libery_indicators/api_connections.py
@@ -1,2 +1,30 @@
-import requests
-import json
+import requests, json
+from datetime import datetime
+from datetime import timedelta
+
+class RequestLibraryRawData(object):
+    def __init__(self, last_update_time):
+        self._get_time = last_update_time
+        self._url = 'http://bibliotecas.cultura.gov.br/api/space/find/'
+        self._filters = {'@select' : 'En_Estado, esfera, esfera_tipo, terms', 'createTimestamp' : "GT("+self._get_time.__str__()+")"}
+        self._response = requests.get(self._url, self._filters)
+        self._data = json.loads(self._response.text)
+
+    @property
+    def response(self):
+        return self._response
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def data_length(self):
+        return len(self._data)
+'''
+#Código para testar o funcionamento da classe.
+get_time = datetime.now() - timedelta(days=50)
+museu = RequestLibraryRawData(get_time)
+print(museu.response.url)
+print(museu.data_length)
+'''
