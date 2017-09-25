1cf47ea8edd7e07db75cb98c05176672412c5790
Lucas Siqueira <lucassiqzro@gmail.com>
2017-09-25 14:41:41 -0300

    Remove repetitive classes

--- libery_indicators/models.py
+++ libery_indicators/models.py
@@ -93,66 +93,6 @@ class PercentLibraryPerAreaOfActivity(Document):
         self._createDate = number
 
 
-class PercentLibraryPerSpherePerState(Document):
-    _totalLibraryPerSpherePerState = MapField(required=True)
-    _totalLibraryPerState = MapField(required=True)
-    _createDate = StringField(required=True)
-
-    @property
-    def totalLibraryPerSpherePerState(self):
-        return self._totalLibraryPerSpherePerState
-
-    @totalLibraryPerSpherePerState.setter
-    def totalLibraryPerSpherePerState(self, number):
-        self._totalLibraryPerSpherePerState = number
-
-    @property
-    def totalLibraryPerState(self):
-        return self._totalLibrary
-
-    @totalLibraryPerState.setter
-    def totalLibraryPerState(self, number):
-        self._totalLibraryPerState = number
-
-    @property
-    def createDate(self):
-        return self._createDate
-
-    @createDate.setter
-    def createDate(self, number):
-        self._createDate = number
-
-
-class PercentLibraryPerSphere(Document):
-    _totalLibraryPerSphere = MapField(required=True)
-    _totalLibrary = MapField(required=True)
-    _createDate = StringField(required=True)
-
-    @property
-    def totalLibraryPerSphere(self):
-        return self._totalLibraryPerSphere
-
-    @totalLibraryPerSphere.setter
-    def totalLibraryPerSphere(self, number):
-        self._totalLibraryPerSphere = number
-
-    @property
-    def totalLibrary(self):
-        return self._totalLibrary
-
-    @totalLibrary.setter
-    def totalLibrary(self, number):
-        self._totalLibrary = number
-
-    @property
-    def createDate(self):
-        return self._createDate
-
-    @createDate.setter
-    def createDate(self, number):
-        self._createDate = number
-
-
 class PercentPublicOrPrivateLibrary(Document):
     _totalPublicLibrary = IntField(required=True)
     _totalPrivateLibrary = IntField(required=True)
