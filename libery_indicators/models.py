2d2561daf383eccd3d8b0a9e54f9eaf63c4e301a
Lucas Siqueira <lucassiqzro@gmail.com>
2017-09-24 23:39:48 -0300

    Create class percent libery for state

--- libery_indicators/models.py
+++ libery_indicators/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from mongoengine import *
 
-# Create your models here.
+class PercentLiberyForState(Document):
+    _totalLiberyForState = MapField(required = True)
+    _totalLibery = IntField(required = True)
+
+    @property
+    def totalLiberyForState(self):
+        return self._totalLiberyForState
+
+    @totalLiberyForState.setter
+    def totalLiberyForState(self, number):
+        self._totalLiberyForState = number
+
+    @property
+    def totalLibery(self):
+        return self._totalLibery
+
+    @totalLibery.setter
+    def totalLibery(self, number):
+        self._totalLibery = number
