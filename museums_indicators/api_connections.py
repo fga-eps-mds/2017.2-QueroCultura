9abeb9df2d3db3752ad9ca3c3aff24d22b9e5b1d
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2017-09-25 03:48:54 -0300

    Add class to request raw data for museums indicators.

--- museums_indicators/api_connections.py
+++ museums_indicators/api_connections.py
@@ -1,2 +1,31 @@
-import requests
-import json
+import requests, json
+from datetime import datetime
+from datetime import timedelta
+
+class RequestMuseumRawData(object):
+    def __init__(self, last_update_time):
+        self._get_time = last_update_time
+        self._url = 'http://museus.cultura.gov.br/api/space/find/'
+        self._filters = {'@select' : 'mus_tipo, mus_tipo_tematica, En_Estado, esfera, mus_servicos_visitaGuiada, mus_arquivo_acessoPublico', 'createTimestamp' : "GT("+self._get_time.__str__()+")"}
+        self._response = requests.get(self._url, self._filters)
+        self._data = json.loads(self._response.text)
+
+    @property
+    def response(self):
+        return self._response
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def data_length(self):
+        return len(self._data)
+
+'''
+#Código para testar o funcionamento da classe.
+get_time = datetime.now() - timedelta(days=50)
+museu = RequestMuseumRawData(get_time)
+print(museu.data[5])
+print(museu.data_length)
+'''
