a56e4c7ce79fe339021e96777108f22c6f860044
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2017-09-25 23:21:50 -0300

    Create class for update raw data for events.

--- events_indicators/api_connections.py
+++ events_indicators/api_connections.py
@@ -1,2 +1,36 @@
-import requests
 import json
+import requests
+# from datetime import datetime, timedelta
+
+
+class RequestEventsRawData(object):
+
+    def __init__(self, last_update_time):
+        self._get_time = last_update_time
+        self._url = 'http://mapas.cultura.gov.br/api/event/find/'
+        self._filters = {'@select': 'classificacaoEtaria, createTimestamp,'
+                         + 'terms, occurrences.{space.{En_Estado}}',
+                         'createTimestamp': "GT("+self._get_time+")"}
+        self._response = requests.get(self._url, self._filters)
+        self._data = json.loads(self._response.text)
+
+    @property
+    def response(self):
+        return self._response
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def data_length(self):
+        return len(self._data)
+
+
+'''
+# Código para testar o funcionamento da classe.
+get_time = datetime.now() - timedelta(days=5)
+museu = RequestEventsRawData(get_time.__str__())
+print(museu.data[3])
+print(museu.data_length)
+'''
