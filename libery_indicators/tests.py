35a593e73bf1c39689b2e30d47a1f11a8c1e445e
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2017-09-26 01:27:28 -0300

    Create tests for librarys raw data class.

--- libery_indicators/tests.py
+++ libery_indicators/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from datetime import datetime
+from .api_connections import RequestLibraryRawData
 
-# Create your tests here.
+
+def test_success_request():
+    current_time = datetime.now().__str__()
+    request_library_raw_data = RequestLibraryRawData(current_time)
+    response_library_raw_data = request_library_raw_data.response
+    response_status_code = response_library_raw_data.status_code
+    assert response_status_code == 200
+
+
+def test_data_content():
+    current_time = datetime.now().__str__()
+    request_museum_raw_data = RequestLibraryRawData(current_time)
+    library_raw_data = request_museum_raw_data.data
+    type_library_raw_data = type(library_raw_data)
+    empty_list = []
+    assert type_library_raw_data == type(empty_list)
+
+
+def test_data_lenght():
+    current_time = datetime.now().__str__()
+    request_library_raw_data = RequestLibraryRawData(current_time)
+    library_raw_data = request_library_raw_data.data_length
+    type_library_raw_data = type(library_raw_data)
+    intenger = 1
+    assert type_library_raw_data == type(intenger)
