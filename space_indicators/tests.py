bb942269f03e8cd9698da50d78b3c5a7adb58a67
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2017-09-26 01:40:06 -0300

    Create tests for spaces raw data class.

--- space_indicators/tests.py
+++ space_indicators/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from datetime import datetime
+from .api_connections import RequestSpacesRawData
 
-# Create your tests here.
+
+def test_success_request():
+    current_time = datetime.now().__str__()
+    request_spaces_raw_data = RequestSpacesRawData(current_time)
+    response_spaces_raw_data = request_spaces_raw_data.response
+    response_status_code = response_spaces_raw_data.status_code
+    assert response_status_code == 200
+
+
+def test_data_content():
+    current_time = datetime.now().__str__()
+    request_spaces_raw_data = RequestSpacesRawData(current_time)
+    spaces_raw_data = request_spaces_raw_data.data
+    type_spaces_raw_data = type(spaces_raw_data)
+    empty_list = []
+    assert type_spaces_raw_data == type(empty_list)
+
+
+def test_data_lenght():
+    current_time = datetime.now().__str__()
+    request_spaces_raw_data = RequestSpacesRawData(current_time)
+    spaces_raw_data = request_spaces_raw_data.data_length
+    type_spaces_raw_data = type(spaces_raw_data)
+    intenger = 1
+    assert type_spaces_raw_data == type(intenger)
