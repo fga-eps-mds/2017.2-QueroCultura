591b9c0d5a05c7adf9db29d79885956a9335b073
pablodiegoss <pablodiegoss@hotmail.com>
2017-10-01 23:44:17 -0300

    Fix events indicators code smells

--- events_indicators/api_connections.py
+++ events_indicators/api_connections.py
@@ -8,7 +8,7 @@ class RequestEventsRawData(object):
         self._get_time = last_update_time
         self._url = 'http://mapas.cultura.gov.br/api/event/find/'
         self._filters = {'@select': 'classificacaoEtaria, createTimestamp,'
-                         + 'terms, occurrences.{space.{En_Estado}}',
+                                    + 'terms, occurrences.{space.{En_Estado}}',
                          'createTimestamp': "GT("+self._get_time+")"}
         self._response = requests.get(self._url, self._filters)
         self._data = json.loads(self._response.text)
