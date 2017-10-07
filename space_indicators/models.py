0098cc20b5420f2d270ee0849cbaf1fe1ec419e9
Josue Nascimento <josuetk63@gmail.com>
2017-10-06 22:55:57 -0300

    Refactor all class models of space_indicators

--- space_indicators/models.py
+++ space_indicators/models.py
@@ -27,180 +27,113 @@ class PercentSpace(Document):
     def create_date(self, number):
         self._create_date = number
 
-class PercentSpacePerType(Document):
-    _totalSpacePerType = DictField(required=True)
+class PercentSpaceState(Document):
+    class Meta:
+        abstract = True
+    _total_space_per_state = DictField(required=True)
+    _create_date = DateTimeField(required=True)
+
+    @property
+    def create_date(self):
+        return self._create_date
+
+    @create_date.setter
+    def create_date(self, number):
+        self._create_date = number
+    @property
+    def total_space_per_state(self):
+        return self._total_space_per_state
+
+    @total_space_per_state.setter
+    def total_space_per_state(self, number):
+        self._total_space_per_state = number
+
+class PercentSpacePerType(PercentSpace):
+    _total_space_per_type = DictField(required=True)
 
 
     @property
     def total_space_per_type(self):
-        return self._totalSpacePerType
+        return self._total_space_per_type
 
     @total_space_per_type.setter
     def total_space_per_type(self, number):
-        self._totalSpacePerType = number
+        self._total_space_per_type = number
 
 
 
 
-class PercentSpacePerOccupationArea(Document):
-    _totalSpacePerOccupationArea = DictField(required=True)
-    _totalSpace = IntField(required=True)
-    _createDate = DateTimeField(required=True)
+class PercentSpacePerOccupationArea(PercentSpace):
+    _total_Space = DictField(required=True)
 
-    @property
-    def total_space(self):
-        return self._totalSpace
-
-    @total_space.setter
-    def total_space(self, number):
-        self._totalSpace = number
 
     @property
     def total_space_per_occupation_area(self):
-        return self._totalSpacePerOccupationArea
+        return self._total_Space
 
     @total_space_per_occupation_area.setter
     def total_space_per_occupation_area(self, number):
-        self._totalSpacePerOccupationArea = number
+        self._total_Space = number
 
-    @property
-    def create_date(self):
-        return self._createDate
-
-    @create_date.setter
-    def create_date(self, number):
-        self._createDate = number
 
 # -------------------- state indicators --------------------------------------
 
 
-class PercentSpacePerTypePerState(Document):
-    _totalSpacePerState = DictField(required=True)
-    _totaSpacePerTypePerState = DictField(required=True)
-    _createDate = DateTimeField(required=True)
+class PercentSpacePerTypePerState(PercentSpaceState):
+    _total_space = DictField(required=True)
 
     @property
-    def total_space_per_state(self):
-        return self._totalSpacePerState
+    def total_space(self):
+        return self._total_space
 
-    @total_space_per_state.setter
-    def total_space_per_state(self, number):
-        self._totalSpacPerStatee = number
+    @total_space.setter
+    def total_space(self, number):
+        self._total_space = number
 
-    @property
-    def total_space_per_type_per_state(self):
-        return self._totalSpacePerTypePerState
 
-    @total_space_per_type_per_state.setter
-    def total_space_per_type_per_state(self, number):
-        self._totalSpacePerTypePerState = number
+class PercentSpacePerOccupationAreaPerState(PercentSpaceState):
+    _total_space_occupation_area_per_state = DictField(required=True)
 
     @property
-    def create_date(self):
-        return self._createDate
+    def total_space_occupation_area_per_state(self):
+        return self._total_space_occupation_area_per_state
 
-    @create_date.setter
-    def create_date(self, number):
-        self._createDate = number
-
-
-class PercentSpacePerOccupationAreaPerState(Document):
-    _totalSpacePerOccupationAreaPerState = DictField(required=True)
-    _totalSpacePerState = DictField(required=True)
-    _createDate = DateTimeField(required=True)
-
-    @property
-    def total_space_per_state(self):
-        return self._totalSpacePerState
-
-    @total_space_per_state.setter
-    def total_space_per_state(self, number):
-        self._totalSpacePerState = number
-
-    @property
-    def total_space_per_occupation_area_per_state(self):
-        return self._totalSpacePerOccupationAreaPerState
-
-    @total_space_per_occupation_area_per_state.setter
-    def total_space_per_occupation_area_per_state(self, number):
-        self._totalSpacePerOccupationArea = number
-
-    @property
-    def create_date(self):
-        return self._createDate
-
-    @create_date.setter
-    def create_date(self, number):
-        self._createDate = number
+    @total_space_occupation_area_per_state.setter
+    def total_space_occupation_area_per_state(self, number):
+        self._total_space_occupation_area_per_state = number
 
 
-class PercentSpaceForState(Document):
-    _totalSpaces = IntField(required=True)
-    _totalSpacePerState = DictField(required=True)
-    _createDate = DateTimeField(required=True)
+class PercentSpaceForState(PercentSpaceState):
+    _total_spaces = IntField(required=True)
 
     @property
     def total_spaces(self):
-        return self._totalSpaces
+        return self._total_spaces
 
     @total_spaces.setter
     def total_spaces(self, number):
-        self._totalSpaces = number
-
-    @property
-    def total_space_per_state(self):
-        return self._totalSpacePerState
-
-    @total_space_per_state.setter
-    def total_space_per_state(self, number):
-        self._totalSpacePerState = number
-
-    @property
-    def create_date(self):
-        return self._createDate
-
-    @create_date.setter
-    def create_date(self, number):
-        self._createDate = number
+        self._total_spaces = number
 
 
 # -------------------- Space Registered --------------------------------------
 
 
-class QuantityOfRegisteredSpace(Document):
-    _totalSpaceRegisteredPerMounthPerYear = DictField(required=True)
-    _totalSpaceRegisteredPerYear = DictField(required=True)
-    _totalSpace = IntField(required=True)
-    _createDate = DateTimeField(required=True)
+class QuantityOfRegisteredSpace(PercentSpace):
+    _total_space_registered_per_mounth_per_year = DictField(required=True)
+    _total_space_register = DictField(required=True)
 
     @property
     def total_space_registered_per_mounth_per_year(self):
-        return self._totalSpaceRegisteredPerMounthPerYear
+        return self._total_space_registered_per_mounth_per_year
 
     @total_space_registered_per_mounth_per_year.setter
     def total_space_registered_per_mounth_per_year(self, number):
-        self._totalSpaceRegisteredPerMounthPerYear = number
+        self._total_space_registered_per_mounth_per_year = number
 
     @property
-    def total_space_registered_per_year(self):
-        return self._totalSpaceRegisteredPerYear
+    def total_space_register(self):
+        return self._total_space_register
 
     @total_space_registered_per_year.setter
-    def total_space_registered_per_year(self, number):
-        self._totalSpaceRegisteredPerYear = number
-
-    @property
-    def total_space(self):
-        return self._totalSpace
-
-    @total_space.setter
-    def total_space(self, number):
-        self._totalSpace = number
-
-    @property
-    def create_date(self):
-        return self._createDate
-
-    @create_date.setter
-    def create_date(self, number):
-        self._createDate = number
+    def ttotal_space_register(self, number):
+        self._total_space_register = number
