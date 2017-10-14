ce43f850c409fb1553a96ec1683a92e95dc4a79e
Josue Nascimento <josuetk63@gmail.com>
2017-10-14 00:10:23 -0300

    Fix indentation of test agent

--- agents_indicators/tests.py
+++ agents_indicators/tests.py
@@ -60,28 +60,28 @@ def test_success_request():
     response_status_code = response_agents_raw_data.status_code
     assert response_status_code == 200
 
-    def test_success_request(self):
-        current_time = datetime.now().__str__()
-        request_agents_raw_data = RequestAgentsRawData(current_time)
-        response_agents_raw_data = request_agents_raw_data.response
-        response_status_code = response_agents_raw_data.status_code
-        assert response_status_code == 200
+def test_success_request(self):
+    current_time = datetime.now().__str__()
+    request_agents_raw_data = RequestAgentsRawData(current_time)
+    response_agents_raw_data = request_agents_raw_data.response
+    response_status_code = response_agents_raw_data.status_code
+    assert response_status_code == 200
 
-    def test_data_content(self):
-        current_time = datetime.now().__str__()
-        request_agents_raw_data = RequestAgentsRawData(current_time)
-        agents_raw_data = request_agents_raw_data.data
-        type_agents_raw_data = type(agents_raw_data)
-        empty_list = []
-        assert type_agents_raw_data == type(empty_list)
+def test_data_content(self):
+    current_time = datetime.now().__str__()
+    request_agents_raw_data = RequestAgentsRawData(current_time)
+    agents_raw_data = request_agents_raw_data.data
+    type_agents_raw_data = type(agents_raw_data)
+    empty_list = []
+    assert type_agents_raw_data == type(empty_list)
 
-    def test_data_lenght(self):
-        current_time = datetime.now().__str__()
-        request_agents_raw_data = RequestAgentsRawData(current_time)
-        agents_raw_data = request_agents_raw_data.data_length
-        type_agents_raw_data = type(agents_raw_data)
-        intenger = 1
-        assert type_agents_raw_data == type(intenger)
+def test_data_lenght(self):
+    current_time = datetime.now().__str__()
+    request_agents_raw_data = RequestAgentsRawData(current_time)
+    agents_raw_data = request_agents_raw_data.data_length
+    type_agents_raw_data = type(agents_raw_data)
+    intenger = 1
+    assert type_agents_raw_data == type(intenger)
 
 def test_data_lenght():
     current_time = datetime.now().__str__()
