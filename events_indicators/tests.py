45ecbeec6c53bf0c6ed23400870017fd71a8bd3a
Josue Nascimento <josuetk63@gmail.com>
2017-10-14 18:37:29 -0300

    Remove self in event indicators

--- events_indicators/tests.py
+++ events_indicators/tests.py
@@ -5,11 +5,10 @@ from .models import PercentEventsPerLanguage
 from .models import PercentEventsPerAgeRange
 
 
-
 class TestQuantityOfRegisteredEvents(object):
 
     @staticmethod
-    def test_total_events_registered_per_mouth_per_year(self):
+    def test_total_events_registered_per_mouth_per_year():
         QuantityOfRegisteredEvents.drop_collection()
         totalEvents = {"mes e ano": "7 2017"}
         event_indicator = QuantityOfRegisteredEvents(totalEvents,{"2017":"março"}, 12 , datetime.now())
@@ -18,7 +17,7 @@ class TestQuantityOfRegisteredEvents(object):
         assert querry._totalEventsRegisteredPerMounthPerYear == totalEvents
 
     @staticmethod
-    def test_total_events_registered_per_year(self):
+    def test_total_events_registered_per_year():
         QuantityOfRegisteredEvents.drop_collection()
         totalEvents = {"mes e ano": "7 2017"}
         event_indicator = QuantityOfRegisteredEvents({"2017":"março"}, totalEvents, 12 , datetime.now())
@@ -27,7 +26,7 @@ class TestQuantityOfRegisteredEvents(object):
         assert querry._totalEventsRegisteredPerYear == totalEvents
 
     @staticmethod
-    def test_total_events(self):
+    def test_total_events():
         QuantityOfRegisteredEvents.drop_collection()
         totalEvents = 20
         event_indicator = QuantityOfRegisteredEvents({"2017":"março"}, {"mes e ano": "7 2017"}, totalEvents , datetime.now())
@@ -38,7 +37,7 @@ class TestQuantityOfRegisteredEvents(object):
 class TestPercentEventsPerLanguage(object):
 
     @staticmethod
-    def test_percent_events_per_language(self):
+    def test_percent_events_per_language():
         PercentEventsPerLanguage.drop_collection()
         total = {'activity area': 20}
         indicator = PercentEventsPerLanguage(total, 20, datetime.now())
@@ -47,7 +46,7 @@ class TestPercentEventsPerLanguage(object):
         assert query._totalEventsPerLanguage == total
 
     @staticmethod
-    def test_total_events(self):
+    def test_total_events():
         PercentEventsPerLanguage.drop_collection()
         total = 50
         indicator = PercentEventsPerLanguage({'activity area': 20}, total, datetime.now())
@@ -59,7 +58,7 @@ class TestPercentEventsPerLanguage(object):
 class TestPercentEventsPerAgeRange(object):
 
     @staticmethod
-    def test_percent_events_per_language(self):
+    def test_percent_events_per_language():
         PercentEventsPerAgeRange.drop_collection()
         total = {'activity area': 20}
         indicator = PercentEventsPerAgeRange(total, 20, datetime.now())
@@ -68,7 +67,7 @@ class TestPercentEventsPerAgeRange(object):
         assert query._totalEventsPerAgeRange == total
 
     @staticmethod
-    def test_total_library(self):
+    def test_total_library():
         PercentEventsPerAgeRange.drop_collection()
         total = 50
         indicator = PercentEventsPerAgeRange({'activity area': 20}, total, datetime.now())
@@ -79,14 +78,14 @@ class TestPercentEventsPerAgeRange(object):
 
 class TestClassRequestEventsRawData(object):
 
-    def test_success_request(self):
+    def test_success_request():
         current_time = datetime.now().__str__()
         request_events_raw_data = RequestEventsRawData(current_time)
         response_events_raw_data = request_events_raw_data.response
         response_status_code = response_events_raw_data.status_code
         assert response_status_code == 200
 
-    def test_data_content(self):
+    def test_data_content():
         current_time = datetime.now().__str__()
         request_events_raw_data = RequestEventsRawData(current_time)
         events_raw_data = request_events_raw_data.data
@@ -94,7 +93,7 @@ class TestClassRequestEventsRawData(object):
         empty_list = []
         assert type_events_raw_data == type(empty_list)
 
-    def test_data_lenght(self):
+    def test_data_lenght():
         current_time = datetime.now().__str__()
         request_events_raw_data = RequestEventsRawData(current_time)
         events_raw_data = request_events_raw_data.data_length
