e6651b08f340455df7e1c2dd3b4e247cc2b7b0b4
pablodiegoss <pablodiegoss@hotmail.com>
2017-10-16 22:21:12 -0200

    Separate map intro 3 files

--- quero_cultura/static/js/map.js
+++ quero_cultura/static/js/map.js
@@ -31,24 +31,6 @@ function InitTime(minutes){
 	return getTimeNow;
 }
 
-function createMarkerIcon(color, extension){
-    filename = ''
-    switch (color) {
-        case 'red': filename = 'markerSpace'
-            break
-        case 'blue': filename = 'markerAgent'
-            break
-        case 'yellow': filename = 'markerEvent'
-            break
-        case 'green': filename = 'markerProject'
-            break
-    }
-
-    return L.icon({ iconUrl: "static/images/"+filename+"."+ extension,
-                    iconSize: [25,25],
-                 });
-}
-
 function MarkersPoints(){
 
     SpaceMarkers("png", 1440); //1440 = 24 x 60, minutes in a day
@@ -63,67 +45,6 @@ function MarkersPoints(){
 
 }
 
-function createPromise(url, type, minutes){
-    var getTimeNow = InitTime(minutes);
-
-    select = ''
-    switch(type){
-        case 'event': select = 'name, occurrences.{space.{location}}, singleUrl'
-            break
-        case 'project': select = 'name, owner.location, singleUrl '
-            break
-        case 'space':
-        case 'agent':
-            select = 'name, location, singleUrl'
-            break
-    }
-
-    var promise = $.getJSON(url,
-      {
-        '@select' : select,
-        '@or' : 1,
-        'createTimestamp' : "GT("+getTimeNow+")",
-        'updateTimestamp' : "GT("+getTimeNow+")"
-      },);
-      return promise
-}
-
-function loadMarkers(type, imageExtension, minutes){
-    loadMarkersInInstance(type,'http://mapas.cultura.gov.br/api/'+type+'/find' ,imageExtension,minutes)
-    loadMarkersInInstance(type,'http://spcultura.prefeitura.sp.gov.br/api/'+type+'/find' ,imageExtension,minutes)
-    loadMarkersInInstance(type,'http://mapa.cultura.ce.gov.br/api/'+type+'/find' ,imageExtension,minutes)
-}
-
-function loadMarkersInInstance(type, url, imageExtension, minutes) {
-    var promise = createPromise(url, type, minutes)
-
-    promise.then(function(data) {
-        switch (type) {
-            case 'project': createProjectMarker(data, imageExtension)
-            break
-            case 'event': createEventMarker(data, imageExtension)
-            break
-            case 'agent': createAgentMarker(data, imageExtension)
-            break
-            case 'space': createSpaceMarker(data, imageExtension)
-            break
-        }
-    });
-}
-
-function createSpaceMarker(data, imageExtension){
-    var redMarker = createMarkerIcon('red', imageExtension)
-
-    for(var i=0; i < data.length; i++){
-        if(data[i]["location"] != null){
-            var marker = L.marker([data[i]["location"]["latitude"],
-                                    data[i]["location"]["longitude"]],
-                                    {icon: redMarker}).addTo(markersSpace);
-            marker.bindPopup('<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+data[i]["singleUrl"]+'>Clique aqui</a>');
-        }
-    }
-}
-
 // creating space markers
 function SpaceMarkers(imageExtension, minutes){
     markersSpace.clearLayers()
@@ -131,20 +52,6 @@ function SpaceMarkers(imageExtension, minutes){
     loadMarkers('space', imageExtension, minutes)
 
     map.addLayer(markersSpace)
-}
-
-// creating Agents markers
-function createAgentMarker(data, imageExtension){
-    var blueMarker = createMarkerIcon('blue', imageExtension)
-
-    for(var i=0; i < data.length; i++){
-    	if(data[i]["location"] != null){
-        	var marker = L.marker([data[i]["location"]["latitude"],
-        							data[i]["location"]["longitude"]],
-        							{icon: blueMarker}).addTo(markersAgent);
-        	marker.bindPopup('<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+data[i]["singleUrl"]+'>Clique aqui</a>');
-    	}
-    }
 }
 
 function AgentMarkers(imageExtension, minutes){
@@ -155,19 +62,6 @@ function AgentMarkers(imageExtension, minutes){
     map.addLayer(markersAgent)
 }
 
-// creating events markers
-function createEventMarker(data, imageExtension){
-    var yellowMarker = createMarkerIcon('yellow', imageExtension)
-
-    for(var i=0; i < data.length; i++){
-    	if((data[i]["occurrences"]).length != 0){
-        	var marker = L.marker([data[i]["occurrences"][0]["space"]["location"]["latitude"],
-        							data[i]["occurrences"][0]["space"]["location"]["longitude"]],
-        							{icon: yellowMarker}).addTo(markersEvent);
-        	marker.bindPopup('<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+data[i]["singleUrl"]+'>Clique aqui</a>');
-    	}
-    }
-}
 
 function EventMarkers(imageExtension, minutes){
     markersEvent.clearLayers()
@@ -178,19 +72,6 @@ function EventMarkers(imageExtension, minutes){
 }
 
 // creating projects markers
-function createProjectMarker(data, imageExtension){
-    var greenMarker = createMarkerIcon('green', imageExtension)
-
-    for(var i=0; i < data.length; i++){
-    	if(data[i]["owner"] != null){
-        	var marker = L.marker([data[i]["owner"]["location"]["latitude"],
-        							data[i]["owner"]["location"]["longitude"]],
-        							{icon: greenMarker}).addTo(markersProject);
-        	marker.bindPopup('<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+data[i]["singleUrl"]+'>Clique aqui</a>');
-    	}
-    }
-
-}
 
 function ProjectMarkers(imageExtension, minutes){
     markersProject.clearLayers()
