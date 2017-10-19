f8a2e17459b68ec9befc4cb044250d0e3534cd69
pablodiegoss <pablodiegoss@hotmail.com>
2017-10-19 13:41:44 -0200

    Update the only test made for the Map

--- quero_cultura/static/js/spec/mapSpec.js
+++ quero_cultura/static/js/spec/mapSpec.js
@@ -1,13 +1,14 @@
 //Testes feitos para o mapa
 describe('InitTime', function () {
-    it('should get time with 3 hours and 5 minutes delay', function () {
-	     var now = InitTime();
-       var timeTest = new Date();
-         timeTest.setHours(timeTest.getHours() - 3, timeTest.getMinutes() - 5);
-         timeTest.setMilliseconds(0);
-         timeTest = timeTest.toJSON()
+	it('should get time with 3 hours and X minutes delay', function () {
+		delay = -5	
+		var now = InitTime(delay);
+		
+		var timeTest = new Date();
+		timeTest.setHours(timeTest.getHours() - 3, timeTest.getMinutes() - delay);	
+		timeTest = timeTest.toJSON()
 
-	     expect(now).toEqual(timeTest);
+		expect(now).toEqual(timeTest);
 
     });
 });
