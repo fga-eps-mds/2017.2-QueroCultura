ee86f49ba6cc11bce14b8a2236ed36d10adc5517
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2017-10-20 20:40:39 -0200

    Create method to generate operation area indicator.

--- agents_indicators/views.py
+++ agents_indicators/views.py
@@ -29,3 +29,20 @@ def build_type_indicator(data):
             per_type[i["type"]["name"]] += 1
 
     return per_type
+
+
+def build_operation_area_indicator(data):
+    per_operation_area = {}
+
+    for i in data:
+        for j in i["terms"]["area"]:
+            if not (j in per_operation_area):
+                per_operation_area[j] = 1
+            else:
+                per_operation_area[j] += 1
+
+    return per_operation_area
+
+# print(build_temporal_indicator(RequestAgentsRawData("2015-05-20 15:47:38.337553", "http://mapas.cultura.gov.br/api/agent/find/").data))
+# print(build_type_indicator(RequestAgentsRawData("2015-05-20 15:47:38.337553", "http://mapas.cultura.gov.br/api/agent/find/").data))
+# print(build_operation_area_indicator(RequestAgentsRawData("2015-05-20 15:47:38.337553", "http://mapas.cultura.gov.br/api/agent/find/").data))
