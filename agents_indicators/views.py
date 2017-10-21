f6d018dbaef78d8198434ecb590d23cf4c713f7a
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2017-10-21 01:38:13 -0200

    Refactor functions for update agents indicators.

--- agents_indicators/views.py
+++ agents_indicators/views.py
@@ -6,10 +6,10 @@ from .models import PercentAgentsPerAreaOperation
 from datetime import datetime
 
 
-def build_temporal_indicator(data):
+def build_temporal_indicator(new_data, old_data):
     temporal_indicator = {}
 
-    for i in data:
+    for i in new_data:
         x = i["createTimestamp"]["date"].split("-")
 
         if not (x[0] in temporal_indicator):
@@ -19,6 +19,16 @@ def build_temporal_indicator(data):
             temporal_indicator[x[0]][x[1]] = 1
         else:
             temporal_indicator[x[0]][x[1]] += 1
+
+    for i in old_data:
+        if not (i in temporal_indicator):
+            temporal_indicator[i] = old_data[i]
+        else:
+            for j in old_data[i]:
+                if not (j in temporal_indicator[i]):
+                    temporal_indicator[i][j] = old_data[i][j]
+                else:
+                    temporal_indicator[i][j] += old_data[i][j]
 
     return temporal_indicator
 
@@ -35,15 +45,21 @@ def build_type_indicator(data):
     return per_type
 
 
-def build_operation_area_indicator(data):
+def build_operation_area_indicator(new_data, old_data):
     per_operation_area = {}
 
-    for i in data:
+    for i in new_data:
         for j in i["terms"]["area"]:
             if not (j in per_operation_area):
                 per_operation_area[j] = 1
             else:
                 per_operation_area[j] += 1
+
+    for i in old_data:
+            if not (i in per_operation_area):
+                per_operation_area[i] = old_data[i]
+            else:
+                per_operation_area[i] += old_data[i]
 
     return per_operation_area
 
@@ -51,13 +67,13 @@ def build_operation_area_indicator(data):
 def update_agent_indicator():
 
     if len(PercentIndividualAndCollectiveAgent.objects) == 0:
-        PercentIndividualAndCollectiveAgent(0, "2010-01-01 15:47:38.337553", 0, 0).save()
+        PercentIndividualAndCollectiveAgent(0, "2012-01-01 15:47:38.337553", 0, 0).save()
 
     if len(PercentAgentsPerAreaOperation.objects) == 0:
-        PercentAgentsPerAreaOperation(0, "2010-01-01 15:47:38.337553", {"00": 0}).save()
+        PercentAgentsPerAreaOperation(0, "2012-01-01 15:47:38.337553", {"Literarura": 0}).save()
 
     if len(AmountAgentsRegisteredPerMonth.objects) == 0:
-        AmountAgentsRegisteredPerMonth({"00": 0}, "2010-01-01 15:47:38.337553").save()
+        AmountAgentsRegisteredPerMonth({"2015": {"01": 0}}, "2012-01-01 15:47:38.337553").save()
 
     index = PercentAgentsPerAreaOperation.objects.count()
 
@@ -70,11 +86,10 @@ def update_agent_indicator():
     new_total = request.data_length + last_per_area.total_agents
     new_create_date = datetime.now().__str__()
 
-    new_per_area = build_operation_area_indicator(request.data)
-
-    new_per_month = build_temporal_indicator(request.data)
-
+    new_per_area = build_operation_area_indicator(request.data, last_per_area.total_agents_area_oreration)
+    new_per_month = build_temporal_indicator(request.data, last_temporal.total_agents_registered_month)
     new_type = build_type_indicator(request.data)
+
     new_individual = last_type.total_individual_agent + new_type["Individual"]
     new_collective = last_type.total_collective_agent + new_type["Coletivo"]
 
