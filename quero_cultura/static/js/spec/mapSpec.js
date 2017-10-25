eaf2d63e76bb9cb8a2140cada4fd3736f373eaee
cjjcastro <cjj.castro3@gmail.com>
2017-10-25 12:35:56 -0200

    Add javascript test in cookie

--- quero_cultura/static/js/spec/mapSpec.js
+++ quero_cultura/static/js/spec/mapSpec.js
@@ -421,3 +421,23 @@ describe('createMarkerIcon', function(){
                  })
 	});
 });
+
+describe('eraseCookie', function(){
+
+	it('should create cookie', function(){
+
+		spyOn(window, 'writeCookie')
+
+		eraseCookie('nome')
+
+		expect(window.writeCookie).toHaveBeenCalledWith('nome', '', -1)
+	});
+});
+
+describe('readCookie', function(){
+	it('should read cookie', function(){
+		var test = readCookie('dontExist01020304')
+
+		expect(test).toEqual(null)
+	});
+});
