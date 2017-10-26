174613638e9a1eecfc0bbef0d90ec0a0252921f8
Josue Nascimento <josuetk63@gmail.com>
2017-10-26 16:46:39 -0200

    Create get area method

--- library_indicators/views.py
+++ library_indicators/views.py
@@ -1,6 +1,6 @@
 from api_connections import RequestLibraryRawData
-from models import PercentLibraries
-from models import PercentPublicOrPrivateLibrary
+#from models import PercentLibraries
+#from models import PercentPublicOrPrivateLibrary
 import datetime
 
 def get_all_libraries():
@@ -9,7 +9,6 @@ def get_all_libraries():
     return libraries
 
 def get_public_libraries():
-
     count = 0
     for librarie in get_all_libraries():
         if librarie["esfera"] != None and librarie["esfera"] == 'Pública':
@@ -32,16 +31,23 @@ def get_undefined_library():
             count = count + 1
     return count
 
+#def update_library_indicator():
+#    if(len(PercentPublicOrPrivateLibrary.objects) == 0):
+#        PercentPublicOrPrivateLibrary(0, "2012-01-01 15:47:38.337553", 0, 0).save()
+#    else:
+#        undefined_library = get_undefined_library()
+#        public_libraries = get_public_libraries()
+#        private_libraries = get_private_libraries()
+#        total_libraries = undefined_library + public_libraries + private_libraries
+#        PercentPublicOrPrivateLibrary(total_libraries, datetime.datetime.now(), public_libraries, private_libraries)
 
-def update_library_indicator():
-    if(len(PercentPublicOrPrivateLibrary.objects) == 0):
-        PercentPublicOrPrivateLibrary(0, "2012-01-01 15:47:38.337553", 0, 0).save()
-    else:
-        undefined_library = get_undefined_library()
-        public_libraries = get_public_libraries()
-        private_libraries = get_private_libraries()
-        total_libraries = undefined_library + public_libraries + private_libraries
-        PercentPublicOrPrivateLibrary(total_libraries, datetime.datetime.now(), public_libraries, private_libraries)
+
+def get_occupation_area():
+    count = 0
+    for librarie in get_all_libraries():
+        for area in librarie["terms"]["area"]:
+            print(area)
 
 
-print(get_undefined_library())
+
+print(get_occupation_area())
