2751b36758e2cc2f9a0257f98c1dd8a424013a88
Caio Oliveira <caiooliv97@gmail.com>
2017-10-26 15:10:30 -0200

    Create get all libraries method

--- library_indicators/views.py
+++ library_indicators/views.py
@@ -1,3 +1,9 @@
-# from django.shortcuts import render
+from api_connections import RequestLibraryRawData
+#from .models import PercentLibraries
+#from .models import PercentPublicOrPrivateLibrary
 
-# Create your views here.
+def get_all_libraries():
+    request = RequestLibraryRawData("2012-01-01 15:47:38.337553")
+
+    libraries = request.response
+    return libraries
