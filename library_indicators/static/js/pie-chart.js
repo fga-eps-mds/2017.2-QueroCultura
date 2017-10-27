3203be67dc4d602a5b953049b7a72b6b98c25143
cjjcastro <cjj.castro3@gmail.com>
2017-10-27 17:49:37 -0200

    Fix pie chart

--- library_indicators/static/js/pie-chart.js
+++ library_indicators/static/js/pie-chart.js
@@ -1,31 +1,31 @@
-var ctx = document.getElementById('pie-chart-library').getContext('2d');
-var chart = new Chart(ctx, {
-  // The type of chart we want to create
-  type: 'pie',
+function pie_chart(){
+  var ctx = document.getElementById('pie-chart-library').getContext('2d');
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'pie',
 
-  // The data for our dataset
-  data: {
-      labels: ["Pública", "Privada"],
-      datasets: [{
-          label: "My First dataset",
-          backgroundColor: [
-            'rgba(255, 51, 51,1)',
-            'rgba(153, 255, 51, 1)',
-
-          ],
-          data: [5, 10],
-      }]
-  },
-
-  // Configuration options go here
-  options: {
-    cutoutPercentage: 50,
-    animation:{
-      animateScale: true,
+    // The data for our dataset
+    data: {
+        labels: ["Pública", "Privada"],
+        datasets: [{
+            label: "My First dataset",
+            data: [10, 10],
+            backgroundColor: [
+              'rgba(255, 51, 51,1)',
+              'rgba(153, 255, 51, 1)',
+            ]
+        }]
     },
-    title: {
-      display: true,
-      text: 'Porcentagem de bibliotecas públicas e privadas'
+    // Configuration options go here
+    options: {
+      cutoutPercentage: 50,
+      animation:{
+        animateScale: true,
+      },
+      title: {
+        display: true,
+        text: 'Porcentagem de bibliotecas públicas e privadas'
+      }
     }
-  }
-});
+  });
+}
