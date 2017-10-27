34b073eaf9f074b4250125b33eb09efdc3d27fdc
cjjcastro <cjj.castro3@gmail.com>
2017-10-27 00:33:48 -0200

    Create library indicator page

--- library_indicators/views.py
+++ library_indicators/views.py
@@ -1,3 +1,8 @@
 # from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+
+def library(request):
+    return render(request, 'library_indicators/library.html')
--- quero_cultura/settings.py
+++ quero_cultura/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'quero_cultura',
     'agents_indicators',
+    'library_indicators',
     'mongoengine',
     'requests',
 ]
