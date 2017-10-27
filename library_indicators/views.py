2a1913d671cd5ec81e7d335f59aa8e087c880494
Josue Nascimento <josuetk63@gmail.com>
2017-10-27 15:19:14 -0200

    Fix save indicator

--- library_indicators/views.py
+++ library_indicators/views.py
@@ -1,8 +1,27 @@
 from api_connections import RequestLibraryRawData
-#from models import PercentLibraries
-#from models import PercentPublicOrPrivateLibrary
-#from models import PercentLibraryPerAreaOfActivity
+from models import PercentLibraries
+from models import PercentPublicOrPrivateLibrary
+from models import PercentLibraryPerAreaOfActivity
 import datetime
+
+
+
+def update_library_public_private_indicator():
+    if(len(PercentPublicOrPrivateLibrary.objects) == 0):
+        PercentPublicOrPrivateLibrary(0, "2012-01-01 15:47:38.337553", 0, 0).save()
+
+    else:
+        undefined_library = get_undefined_library()
+        public_libraries = get_public_libraries()
+        private_libraries = get_private_libraries()
+        total_libraries = undefined_library + public_libraries + private_libraries
+        PercentPublicOrPrivateLibrary(total_libraries, datetime.datetime.now(), public_libraries, private_libraries).save()
+
+
+def update_library_per_area_activity():
+     if(len(PercentLibraryPerAreaOfActivity.objects) == 0):
+         pass
+
 
 def get_all_libraries():
     request = RequestLibraryRawData("2012-01-01 15:47:38.337553")
@@ -32,23 +51,6 @@ def get_undefined_library():
             count = count + 1
     return count
 
-#def update_library_public_private_indicator():
-#    if(len(PercentPublicOrPrivateLibrary.objects) == 0):
-#        PercentPublicOrPrivateLibrary(0, "2012-01-01 15:47:38.337553", 0, 0).save()
-#
-#    else:
-#        undefined_library = get_undefined_library()
-#        public_libraries = get_public_libraries()
-#        private_libraries = get_private_libraries()
-#        total_libraries = undefined_library + public_libraries + private_libraries
-#        PercentPublicOrPrivateLibrary(total_libraries, datetime.datetime.now(), public_libraries, private_libraries)
-
-
-#def update_library_per_area_activity():
-#     if(len(PercentLibraryPerAreaOfActivity.objects) == 0):
-#
-#
-#
 
 def get_all_occupation_area():
     areas = {}
