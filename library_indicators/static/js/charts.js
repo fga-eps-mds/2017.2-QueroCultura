15ce588111d2c91cc82fbfbfe0cdb701536807ae
cjjcastro <cjj.castro3@gmail.com>
2017-10-27 22:41:33 -0200

    Fix charts function in library indicator

--- library_indicators/static/js/charts.js
+++ library_indicators/static/js/charts.js
@@ -1,4 +1,4 @@
-function pie_chart(){
+function public_private_chart(amount_public_libraries, amount_private_libraries){
   var ctx = document.getElementById('pie-chart-library').getContext('2d');
   var chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -8,10 +8,10 @@ function pie_chart(){
     data: {
         labels: ["Pública", "Privada"],
         datasets: [{
-            label: "My First dataset",
-            data: [10, 10],
+            label: "Points",
+            data: [amount_public_libraries, amount_private_libraries],
             backgroundColor: [
-              'rgba(255, 51, 51,1)',
+              'rgba(255, 51, 51, 1)',
               'rgba(153, 255, 51, 1)',
             ]
         }]
