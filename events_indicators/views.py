c978555b4464deac48c42e445770b0ca3438c8e9
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2017-10-29 14:36:54 -0200

    Config events page.

--- events_indicators/views.py
+++ events_indicators/views.py
@@ -1,3 +1,5 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'events_indicators/events_indicators.html')
--- quero_cultura/test_settings.py
+++ quero_cultura/test_settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mongoengine',
     'agents_indicators',
+    'events_indicators',
     'requests',
 ]
 
