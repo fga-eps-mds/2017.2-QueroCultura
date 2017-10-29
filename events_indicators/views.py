161c10f71ab4f90a464c9cac16c94929660b01cd
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2017-10-29 15:30:47 -0200

    Create per age range indicator

--- events_indicators/views.py
+++ events_indicators/views.py
@@ -8,6 +8,18 @@ def index(request):
 def build_age_range_indicator(new_data, old_data):
     per_age_range = {}
 
+    for event in new_data:
+        if not (event["classificacaoEtaria"] in per_age_range):
+            per_age_range[event["classificacaoEtaria"]] = 1
+        else:
+            per_age_range[event["classificacaoEtaria"]] += 1
+
+    for age_range in old_data:
+        if not (age_range in per_age_range):
+            per_age_range[age_range] = old_data[age_range]
+        else:
+            per_age_range[age_range] += old_data[age_range]
+
     return per_age_range
 
 
