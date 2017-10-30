6d0567dfc17590fb0572f1200e7a06788e5536d8
cjjcastro <cjj.castro3@gmail.com>
2017-10-30 02:29:16 -0200

    Add charts of quantity per year and mouth

--- library_indicators/static/js/charts.js
+++ library_indicators/static/js/charts.js
@@ -79,3 +79,43 @@ function bar_chart(indicators, chartId, labelName, description){
                         });
 
 }
+
+function line_chart(indicators, chartId, labelName, description){
+        var ctx = document.getElementById(chartId);
+        var name_growth = [];
+        var temp_growth = [];
+        jQuery.each(indicators, function(key, value){
+          name_growth.push(key);
+          temp_growth.push(value);
+        });
+
+        var agentchart = new Chart(ctx, {
+                                type: 'line',
+                                data: {labels: name_growth,
+                                       datasets: [{
+                                                    label: labelName,
+                                                    data: temp_growth,
+                                                    fill: false,
+                                                    borderWidth: 2.5,
+                                                    borderColor: 'rgba(77,166,253,0.85)',
+                                                    backgroundColor: 'transparent',
+                            	                    bordeCapStyle: 'butt',
+                        	                        borderDash: [],
+                                	                borderJoinStyle: 'miter',
+                                	                pointBorderColor: "rgba(75,192,192,1",
+                                	                pointBackgroundColor: "fff",
+                            	                    pointBorderWidth: 3,
+                                	                pointHoverRadius: 5,
+                            	                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                                	                pointHoverBorderColor: "rgba(220,220,220,1)",
+                                	                pointHoverBorderWidth: 2,
+                                	                pointRadius: 1,
+                                	                pointHitRadius: 10
+                                                }]
+                                        },
+                                options: {title: {display: true,
+                                                  text: description
+                                                }
+                                        }
+                        });
+}
