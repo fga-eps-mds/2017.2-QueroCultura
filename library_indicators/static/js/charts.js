09bd1b98dba62e96c3e242dfecb044c6e57954b4
cjjcastro <cjj.castro3@gmail.com>
2017-10-30 01:35:17 -0200

    Add library chart by area

--- library_indicators/static/js/charts.js
+++ library_indicators/static/js/charts.js
@@ -1,3 +1,18 @@
+function poolColors(a) {
+    var pool = [];
+    for(i=0;i<a;i++){
+        pool.push(dynamicColors());
+    }
+    return pool;
+}
+
+function dynamicColors() {
+    var r = Math.floor(Math.random() * 255);
+    var g = Math.floor(Math.random() * 255);
+    var b = Math.floor(Math.random() * 255);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
+
 function public_private_chart(amount_public_libraries, amount_private_libraries){
   var ctx = document.getElementById('pie-chart-library').getContext('2d');
   var chart = new Chart(ctx, {
@@ -28,4 +43,41 @@ function public_private_chart(amount_public_libraries, amount_private_libraries)
       }
     }
   });
+}
+
+function per_area_chart(ocupation_area_total){
+  var ctxArea = document.getElementById("bar-chart-area");
+  console.log(ocupation_area_total);
+  var nameArea = [];
+  var amountArea = [];
+  jQuery.each(ocupation_area_total, function(key, value){
+    nameArea.push(key);
+    amountArea.push(value);
+  });
+
+
+  console.log(ocupation_area_total);
+  var agentB = new Chart(ctxArea, {
+                          type: 'bar',
+                          data: {
+                              labels: nameArea,
+                              datasets: [{
+                                  label: 'Área de atuação',
+                                  data: amountArea,
+                                  backgroundColor: poolColors(amountArea.length)
+                              }]
+                          },
+                          options: {
+                              scales: {
+                                  yAxes: [{
+                                      ticks: {beginAtZero:true}
+                                 }]
+                             },
+                             legend: {display: true},
+                             title: {display: true,
+                                     text: 'Quantidade de registros por área de atuação'
+                             }
+                          }
+                      });
+
 }
