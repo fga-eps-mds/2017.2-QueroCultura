77bbe4ad033b75985f5afc6d38102a845350d911
cjjcastro <cjj.castro3@gmail.com>
2017-10-30 22:48:29 -0200

    Refactor chartJS use to remove Duplicate code

--- library_indicators/static/js/charts.js
+++ library_indicators/static/js/charts.js
@@ -1,121 +1,46 @@
-function poolColors(a) {
-    var pool = [];
-    for(i=0;i<a;i++){
-        pool.push(dynamicColors());
-    }
-    return pool;
-}
-
-function dynamicColors() {
-    var r = Math.floor(Math.random() * 255);
-    var g = Math.floor(Math.random() * 255);
-    var b = Math.floor(Math.random() * 255);
-    return "rgb(" + r + "," + g + "," + b + ")";
-}
-
 function public_private_chart(amount_public_libraries, amount_private_libraries){
-  var ctx = document.getElementById('pie-chart-library').getContext('2d');
-  var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'pie',
+    var keys = ["Pública", "Privada"]
+    var values = [amount_public_libraries, amount_private_libraries]
+    var context = document.getElementById('pie-chart-library').getContext('2d');
 
-    // The data for our dataset
-    data: {
-        labels: ["Pública", "Privada"],
-        datasets: [{
-            label: "Points",
-            data: [amount_public_libraries, amount_private_libraries],
-            backgroundColor: [
-              'rgba(255, 51, 51, 1)',
-              'rgba(153, 255, 51, 1)',
-            ]
-        }]
-    },
-    // Configuration options go here
-    options: {
-      cutoutPercentage: 50,
-      animation:{
-        animateScale: true,
-      },
-      title: {
-        display: true,
-        text: 'Porcentagem de bibliotecas públicas e privadas'
-      }
-    }
-  });
+    var label = "Points"
+    var description = 'Porcentagem de bibliotecas públicas e privadas'
+    var category = "pie"
+
+    createChart(category, context, keys, values, label, description)
 }
 
-function bar_chart(indicators, chartId, labelName, description){
-    var ctxArea = document.getElementById(chartId);
+function bar_chart(indicators, chartId, label, description){
+    indicators = convertIndicators(indicators)
+    var keys = indicators.keys;
+    var values = indicators.values;
 
-    var nameArea = [];
-    var amountArea = [];
+    var context = document.getElementById(chartId);
+
+    var category = 'bar'
+    createChart(category, context, keys, values, label, description)
+}
+
+function line_chart(indicators, chartId, label, description){
+    indicators = convertIndicators(indicators)
+    var keys = indicators.keys;
+    var values = indicators.values;
+
+    var context = document.getElementById(chartId);
+
+    var category = "line"
+    createChart(category, context, keys, values, label, description)
+}
+
+function convertIndicators(indicators){
+    var keys = [];
+    var values = [];
+
     jQuery.each(indicators, function(key, value){
-      nameArea.push(key);
-      amountArea.push(value);
+      keys.push(key);
+      values.push(value);
     });
 
-    var chart = new Chart(ctxArea, {
-                            type: 'bar',
-                            data: {
-                                labels: nameArea,
-                                datasets: [{
-                                    label: labelName,
-                                    data: amountArea,
-                                    backgroundColor: poolColors(amountArea.length)
-                                }]
-                            },
-                            options: {
-                                scales: {
-                                    yAxes: [{
-                                        ticks: {beginAtZero:true}
-                                   }]
-                               },
-                               legend: {display: true},
-                               title: {display: true,
-                                       text: description
-                               }
-                            }
-                        });
-
-}
-
-function line_chart(indicators, chartId, labelName, description){
-        var ctx = document.getElementById(chartId);
-        var name_growth = [];
-        var temp_growth = [];
-        jQuery.each(indicators, function(key, value){
-          name_growth.push(key);
-          temp_growth.push(value);
-        });
-
-        var agentchart = new Chart(ctx, {
-                                type: 'line',
-                                data: {labels: name_growth,
-                                       datasets: [{
-                                                    label: labelName,
-                                                    data: temp_growth,
-                                                    fill: false,
-                                                    borderWidth: 2.5,
-                                                    borderColor: 'rgba(77,166,253,0.85)',
-                                                    backgroundColor: 'transparent',
-                            	                    bordeCapStyle: 'butt',
-                        	                        borderDash: [],
-                                	                borderJoinStyle: 'miter',
-                                	                pointBorderColor: "rgba(75,192,192,1",
-                                	                pointBackgroundColor: "fff",
-                            	                    pointBorderWidth: 3,
-                                	                pointHoverRadius: 5,
-                            	                    pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                                	                pointHoverBorderColor: "rgba(220,220,220,1)",
-                                	                pointHoverBorderWidth: 2,
-                                	                pointRadius: 1,
-                                	                pointHitRadius: 10
-                                                }]
-                                        },
-                                options: {title: {display: true,
-                                                  text: description
-                                                }
-                                        }
-                        });
+    var newIndicators = {keys: keys, values:values}
+    return newIndicators
 }
