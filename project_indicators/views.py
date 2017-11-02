ca6e18bfb2c68fb3035f9575cae74ef1a44e0080
cjjcastro <cjj.castro3@gmail.com>
2017-11-02 21:45:38 -0200

    Configure initial page from project indicator

--- project_indicators/views.py
+++ project_indicators/views.py
@@ -1,3 +1,12 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+
 
 # Create your views here.
+
+def index(request):
+
+    # Cria dicionario para apresentação dos graficos de indicadores
+    context = {}
+
+    # Renderiza pagina e envia dicionario para apresentação dos graficos
+    return render(request, 'project_indicators/project-indicators.html', context)
