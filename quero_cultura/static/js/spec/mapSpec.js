bf727707d5c4c4655527e73b4f7cf69c2fb41270
Lucas Siqueira <lucassiqzro@gmail.com>
2017-11-02 02:54:26 -0200

    Create javascript tests in map

--- quero_cultura/static/js/spec/mapSpec.js
+++ quero_cultura/static/js/spec/mapSpec.js
@@ -286,3 +286,20 @@ describe('getInitialInstance', function(){
 		expect(test).toEqual(splitUrl[2])
 	});
 });
+
+describe('makeIdForMarker', function(){
+		it('Should call getInitialInstance', function(){
+			spyOn(window,'getInitialInstance')
+			data = [{'id': 1, 'singleUrl': "http://spcultura.prefeitura.sp.gov.br/projeto/3304/"}]
+			position = 0
+			makeIdForMarker(data,position)
+			expect(window.getInitialInstance).toHaveBeenCalled()
+		});
+		it('Should return identification', function(){
+			data = [{'id': 1, 'singleUrl': "http://spcultura.prefeitura.sp.gov.br/projeto/3304/"}]
+			position = 0
+			identification = makeIdForMarker(data,position)
+			equal = 'sp1'
+			expect(identification).toEqual(equal)
+		});
+});
