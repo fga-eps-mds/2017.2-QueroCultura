1ca89c2f3ad1aa52bef99919bfd9b285bb10c057
Jefersonalves <jefersonalvesf2009@gmail.com>
2017-11-03 20:21:23 -0200

    Add subsite link for project, space and events

--- quero_cultura/static/js/marker_creators.js
+++ quero_cultura/static/js/marker_creators.js
@@ -1,7 +1,35 @@
 var newMarkers = new Map()
 var printedFeed = new Map()
 var diffFeed = new Map()
-var subsites = {1:"museus.cultura.gov.br",2:"bibliotecas.cultura.gov.br",4:"mapas.cultura.gov.br",9:"mapacultural.es.gov.br",11:"pb.mapas.cultura.gov.br",12:"ma.mapas.cultura.gov.br",15:"pa.mapas.cultura.gov.br",16:"jaguarao.rs.mapas.cultura.gov.br",17:"meleva.cultura.gov.br",18:"grucultura.guarulhos.sp.gov.br",20:"laguna.sc.mapas.cultura.gov.br",21:"mapa.cultura.aracaju.se.gov.br",22:"francodarocha.sp.mapas.cultura.gov.br",23:"mapacultural.itu.sp.gov.br",24:"ba.mapas.cultura.gov.br",25:"mapacultural.parnaiba.pi.gov.br",26:"osasco.sp.mapas.cultura.gov.br",27:"camacari.ba.mapas.cultura.gov.br",28:"ilheus.ba.mapas.cultura.gov.br",29:"varzeagrande.mt.mapas.cultura.gov.br",30:"pontosdememoria.cultura.gov.br",31:"mapadaculturafoz.pmfi.pr.gov.br",32:"senhordobonfim.ba.mapas.cultura.gov.br",33:"mapas.cultura.se.gov.br",34:"mapacultural.itapetininga.sp.gov.br",35:"mapacultural.ipatinga.mg.gov.br",36:"mapacultural.novohamburgo.rs.gov.br",37:"mapacultural.saocaetanodosul.sp.gov.br"}
+
+var subsites = {1:"museus.cultura.gov.br",
+                2:"bibliotecas.cultura.gov.br",
+                4:"mapas.cultura.gov.br",
+                9:"mapacultural.es.gov.br",
+                11:"pb.mapas.cultura.gov.br",
+                12:"ma.mapas.cultura.gov.br",
+                15:"pa.mapas.cultura.gov.br",
+                16:"jaguarao.rs.mapas.cultura.gov.br",
+                17:"meleva.cultura.gov.br",
+                18:"grucultura.guarulhos.sp.gov.br",
+                20:"laguna.sc.mapas.cultura.gov.br",
+                21:"mapa.cultura.aracaju.se.gov.br",
+                22:"francodarocha.sp.mapas.cultura.gov.br",
+                23:"mapacultural.itu.sp.gov.br",
+                24:"ba.mapas.cultura.gov.br",
+                25:"mapacultural.parnaiba.pi.gov.br",
+                26:"osasco.sp.mapas.cultura.gov.br",
+                27:"camacari.ba.mapas.cultura.gov.br",
+                28:"ilheus.ba.mapas.cultura.gov.br",
+                29:"varzeagrande.mt.mapas.cultura.gov.br",
+                30:"pontosdememoria.cultura.gov.br",
+                31:"mapadaculturafoz.pmfi.pr.gov.br",
+                32:"senhordobonfim.ba.mapas.cultura.gov.br",
+                33:"mapas.cultura.se.gov.br",
+                34:"mapacultural.itapetininga.sp.gov.br",
+                35:"mapacultural.ipatinga.mg.gov.br",
+                36:"mapacultural.novohamburgo.rs.gov.br",
+                37:"mapacultural.saocaetanodosul.sp.gov.br"}
 
 var printedMarkers = Array()
 
@@ -80,9 +108,17 @@ function createSpaceMarker(data, imageExtension){
             var idForMarker = makeIdForMarker(data,i)
             newMarkers.set(idForMarker, data[i])
             var marker = L.marker([data[i]["location"]["latitude"],
-                                    data[i]["location"]["longitude"]],
-                                    {icon: redMarker}).setZIndexOffset(valueZindex).addTo(markersSpace);
-            marker.bindPopup('<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+data[i]["singleUrl"]+'>Clique aqui</a>');
+                                   data[i]["location"]["longitude"]],
+                                   {icon: redMarker}).setZIndexOffset(valueZindex).addTo(markersSpace);
+
+            //Verify if exist an subsite, if exist, change url to subsite 
+            if(data[i]['subsite'] != null){
+                linkSubsite = "http://"+subsites[data[i]['subsite']] + "/espaco/" + data[i]["id"]
+                marker.bindPopup('<h6><b>Subsite:</b></h6>'+data[i]["subsite"]+'<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+linkSubsite+'>Clique aqui</a>');
+            }else{
+                marker.bindPopup('<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+data[i]["singleUrl"]+'>Clique aqui</a>');
+            }           
+
             identifiedMarker = {"id" : data[i].id,"marker" : marker}
             printedMarkers.push(identifiedMarker)
         }
@@ -99,15 +135,18 @@ function createAgentMarker(data, imageExtension){
             var valueZindex = setZIndex(imageExtension)
             newMarkers.set(idForMarker, data[i])
             var marker = L.marker([data[i]["location"]["latitude"],
-            data[i]["location"]["longitude"]],
-            {icon: blueMarker}).setZIndexOffset(valueZindex).addTo(markersAgent)
+                                   data[i]["location"]["longitude"]],
+                                   {icon: blueMarker}).setZIndexOffset(valueZindex).addTo(markersAgent)
+
+            //Verify if exist an subsite, if exist, change url to subsite 
             if(data[i]['subsite'] != null){
-                linkSubsite = "http://www."+subsites[data[i]['subsite']] + "/agente/" + data[i]["id"]
+                linkSubsite = "http://"+subsites[data[i]['subsite']] + "/agente/" + data[i]["id"]
                 marker.bindPopup('<h6><b>Subsite:</b></h6>'+data[i]["subsite"]+'<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+linkSubsite+'>Clique aqui</a>');
-                console.log(linkSubsite);
+                //console.log(linkSubsite);
             }else{
                 marker.bindPopup('<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+data[i]["singleUrl"]+'>Clique aqui</a>');
             }
+
             identifiedMarker = {"id" : data[i].id,"marker" : marker}
             printedMarkers.push(identifiedMarker)
           }
@@ -125,9 +164,18 @@ function createEventMarker(data, imageExtension){
             var valueZindex = setZIndex(imageExtension)
             newMarkers.set(idForMarker, data[i])
             var marker = L.marker([data[i]["occurrences"][0]["space"]["location"]["latitude"],
-            data[i]["occurrences"][0]["space"]["location"]["longitude"]],
-            {icon: yellowMarker}).setZIndexOffset(valueZindex).addTo(markersEvent);
-            marker.bindPopup('<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+data[i]["singleUrl"]+'>Clique aqui</a>');
+                                   data[i]["occurrences"][0]["space"]["location"]["longitude"]],
+                                   {icon: yellowMarker}).setZIndexOffset(valueZindex).addTo(markersEvent);
+
+            //Verify if exist an subsite, if exist, change url to subsite
+            if(data[i]['subsite'] != null){
+                linkSubsite = "http://"+subsites[data[i]['subsite']] + "/evento/" + data[i]["id"]
+                marker.bindPopup('<h6><b>Subsite:</b></h6>'+data[i]["subsite"]+'<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+linkSubsite+'>Clique aqui</a>');
+                //console.log(linkSubsite)
+            }else{
+                marker.bindPopup('<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+data[i]["singleUrl"]+'>Clique aqui</a>');
+            }
+
             identifiedMarker = {"id" : data[i].id,"marker" : marker}
             printedMarkers.push(identifiedMarker)
 
@@ -143,14 +191,22 @@ function createProjectMarker(data, imageExtension){
           data[i]["type"] = "project"
           var idForMarker = makeIdForMarker(data,i)
           var valueZindex = setZIndex(imageExtension)
-
           newMarkers.set(idForMarker, data[i])
         	var marker = L.marker([data[i]["owner"]["location"]["latitude"],
-        							data[i]["owner"]["location"]["longitude"]],
-        							{icon: greenMarker}).setZIndexOffset(valueZindex).addTo(markersProject);
-        	marker.bindPopup('<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+data[i]["singleUrl"]+'>Clique aqui</a>');
-            identifiedMarker = {"id" : data[i].id,"marker" : marker}
-            printedMarkers.push(identifiedMarker)
+        							           data[i]["owner"]["location"]["longitude"]],
+        							           {icon: greenMarker}).setZIndexOffset(valueZindex).addTo(markersProject);
+          
+          //Verify if exist an subsite, if exist, change url to subsite
+          if(data[i]['subsite'] != null){
+              linkSubsite = "http://"+subsites[data[i]['subsite']] + "/projeto/" + data[i]["id"]
+              marker.bindPopup('<h6><b>Subsite:</b></h6>'+data[i]["subsite"]+'<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+linkSubsite+'>Clique aqui</a>');
+              console.log(linkSubsite)
+          }else{
+              marker.bindPopup('<h6><b>Nome:</b></h6>'+data[i]["name"]+'<h6><b>Link:</b></h6><a target="_blank" href='+data[i]["singleUrl"]+'>Clique aqui</a>');
+          }
+
+          identifiedMarker = {"id" : data[i].id,"marker" : marker}
+          printedMarkers.push(identifiedMarker)
         }
       }
 }
