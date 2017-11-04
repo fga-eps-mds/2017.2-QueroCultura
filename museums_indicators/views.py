606c238872200f492f1fe785575de2e5a080019f
m7rodrigues <matheus.1nascimento58@gmail.com>
2017-11-04 20:19:44 -0200

    Create method for build per type indicator

--- museums_indicators/views.py
+++ museums_indicators/views.py
@@ -19,3 +19,20 @@ def build_thematic_indicator(new_data, old_data):
 
     return per_thematic
 
+
+def build_per_type_indicator(new_data, old_data):
+    per_type = {}
+
+    for museum in new_data:
+        if not (str(museum["mus_tipo"]) in per_type):
+            per_type[str(museum["mus_tipo"])] = 1
+        else:
+            per_type[str(museum["mus_tipo"])] += 1
+
+    for museum in old_data:
+        if not (museum in per_type):
+            per_type[museum] = old_data[museum]
+        else:
+            per_type[museum] += old_data[museum]
+
+    return per_type
