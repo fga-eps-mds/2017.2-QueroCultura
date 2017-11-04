c59c8922e1709e0dbed26011f51be0144e723df0
m7rodrigues <matheus.1nascimento58@gmail.com>
2017-11-04 21:04:56 -0200

    Create method build museums publics and privates

--- museums_indicators/views.py
+++ museums_indicators/views.py
@@ -36,3 +36,23 @@ def build_per_type_indicator(new_data, old_data):
             per_type[museum] += old_data[museum]
 
     return per_type
+
+
+def build_public_private_indicator(new_data, old_data):
+    per_public_private = {}
+
+    for museum in new_data:
+        if not (str(museum["esfera"]) in per_public_private):
+            per_public_private[str(museum["esfera"])] = 1
+        else:
+            per_public_private[str(museum["esfera"])] += 1
+
+    for museum in old_data:
+        if not (museum in per_public_private):
+            per_public_private[museum] = old_data[museum]
+        else:
+            per_public_private[museum] += old_data[museum]
+
+    return per_public_private
+
+
