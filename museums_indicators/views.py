d2adcb2a3b1f503097f2a40476640db2d40684c9
Hugo Carvalho <hugo-nc@hotmail.com>
2017-11-06 22:31:44 -0200

    Add methods to load new and old indicators
    Signed-off-by: Hugo Carvalho <hugo-nc@hotmail.com>
    Signed-off-by: Matheus Rodriguês <matheus.1nascimento58@gmail.com>

--- museums_indicators/views.py
+++ museums_indicators/views.py
@@ -1,58 +1,57 @@
 from django.shortcuts import render
-from .api_connections import RequestEventsRawData
+from .api_connections import RequestMuseumRawData
+from .models import PercentThematicsMuseums
+from .models import PercentTypeMuseums
+from .models import PercentPublicOrPrivateMuseums
+from .models import AmountMuseumsRegisteredYear
+from .models import PercentMuseumsPromoteGuidedTour
+from .models import PercentMuseumsHistoricalArchivePublicAccess
+from quero_cultura.views import build_temporal_indicator
+from quero_cultura.views import build_simple_indicator
+
+#Busca os indicadores já cadastrados
+def load_current_musuem_indicators():
+    indicators = []
+    index = PercentTypeMuseums.objects.count()
+
+    #Busca no banco os ultimos cadastros
+    last_per_type = PercentTypeMuseums.objects[index - 1]
+    last_per_thematic = PercentThematicsMuseums.objects[index - 1]
+    last_per_sphere = PercentPublicOrPrivateMuseums.objects[index - 1]
+    last_temporal = AmountMuseumsRegisteredYear.objects[index - 1]
+    last_guided_tour = PercentMuseumsPromoteGuidedTour.objects[index - 1]
+    last_public_archive = PercentMuseumsHistoricalArchivePublicAccess.objects[index - 1]
+
+    new_total = last_per_type.total_museums
+    last_update_date = last_per_type.create_date
+
+    #Adiciona as informações na lista de indicadores
+    indicators.append(last_temporal.total_museums_registered_year)
+    indicators.append(last_per_type.type_museums)
+    indicators.append(last_per_thematic.thmeatics_museums)
+    indicators.append(last_per_sphere.total_public_private_museums)
+    indicators.append(last_guided_tour.total_museums_promote_guide)
+    indicators.append(last_public_archive.total_museums_historical)
+
+    return new_total, last_update_date, indicators
+
+#faz a request
+def request_museum_new_data(new_total, last_update_date, temporal_indicator):
+    indicators = []
+
+    request = RequestMuseumRawData(last_update_date)
+    new_total += request.data_length
+
+    #O indicador temporal necessita de new e old data, sendo assim, há o indicador é calculado aqui.
+    indicators.append(build_temporal_indicator(request.data, temporal_indicator))
+
+    indicators.append(build_simple_indicator(request.data, "mus_tipo"))
+    indicators.append(build_simple_indicator(request.data, "mus_tipo_tematica"))
+    indicators.append(build_simple_indicator(request.data, "esfera"))
+    indicators.append(build_simple_indicator(request.data, "mus_servicos_visitaGuiada"))
+    indicators.append(build_simple_indicator(request.data, "mus_arquivo_acessoPublico"))
 
 
-def build_thematic_indicator(new_data, old_data):
-    per_thematic = {}
+    return new_total, indicators
 
-    for museum in new_data:
-        if not (str(museum["mus_tipo_tematica"]) in per_thematic):
-            per_thematic[str(museum["mus_tipo_tematica"])] = 1
-        else:
-            per_thematic[str(museum["mus_tipo_tematica"])] += 1
-
-    for thematic in old_data:
-        if not (thematic in per_thematic):
-            per_thematic[thematic] = old_data[thematic]
-        else:
-            per_thematic[thematic] += old_data[thematic]
-
-    return per_thematic
-
-
-def build_per_type_indicator(new_data, old_data):
-    per_type = {}
-
-    for museum in new_data:
-        if not (str(museum["mus_tipo"]) in per_type):
-            per_type[str(museum["mus_tipo"])] = 1
-        else:
-            per_type[str(museum["mus_tipo"])] += 1
-
-    for museum in old_data:
-        if not (museum in per_type):
-            per_type[museum] = old_data[museum]
-        else:
-            per_type[museum] += old_data[museum]
-
-    return per_type
-
-
-def build_public_private_indicator(new_data, old_data):
-    per_public_private = {}
-
-    for museum in new_data:
-        if not (str(museum["esfera"]) in per_public_private):
-            per_public_private[str(museum["esfera"])] = 1
-        else:
-            per_public_private[str(museum["esfera"])] += 1
-
-    for museum in old_data:
-        if not (museum in per_public_private):
-            per_public_private[museum] = old_data[museum]
-        else:
-            per_public_private[museum] += old_data[museum]
-
-    return per_public_private
-
-
+#consilidação do indicador
