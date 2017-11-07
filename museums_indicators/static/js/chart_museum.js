c3831d7b4924bdfbc9ff5d35304720c35d5c51f6
m7rodrigues <matheus.1nascimento58@gmail.com>
2017-11-07 04:35:42 -0200

    Create charts museums indicators
    Signed-off-by: m7rodrigues <matheus.1nascimento58@gmail.com>
    Signed-off-by: Hugo-Carvalho <hugo-nc@hotmail.com>

--- museums_indicators/static/js/chart_museum.js
+++ museums_indicators/static/js/chart_museum.js
@@ -15,3 +15,54 @@ function growth_line_graphic(data_keys ,data_values){
 
     createChart(graphics_data)
 }
+
+function type_bar_graphic(data_keys ,data_values){
+    var graphics_data = createDefaultData("bar", data_keys, data_values)
+
+    graphics_data.label = 'Quantidades de registros por tipo'
+    graphics_data.context = document.getElementById("bar-chart-type")
+    graphics_data.description = 'Quantidades de Registros por tipo'
+
+    createChart(graphics_data)
+}
+
+function thematic_bar_graphic(data_keys ,data_values){
+    var graphics_data = createDefaultData("bar", data_keys, data_values)
+
+    graphics_data.label = 'Quantidades de registros por tipo'
+    graphics_data.context = document.getElementById("bar-chart-thematic")
+    graphics_data.description = 'Quantidades de Registros por tipo'
+
+    createChart(graphics_data)
+}
+
+
+function archive_pie_graphic(data_keys, data_values){
+    var graphics_data = createDefaultData("pie", data_keys, data_values)
+
+    graphics_data.label = "Points"
+    graphics_data.context = document.getElementById("pie-chart-archive").getContext('2d')
+    graphics_data.description = 'Porcentagem de agentes individuais e coletivos'
+
+    createChart(graphics_data)
+}
+
+function guided_pie_graphic(data_keys, data_values){
+    var graphics_data = createDefaultData("pie", data_keys, data_values)
+
+    graphics_data.label = "Points"
+    graphics_data.context = document.getElementById("pie-chart-guided").getContext('2d')
+    graphics_data.description = 'Porcentagem de agentes individuais e coletivos'
+
+    createChart(graphics_data)
+}
+
+function sphere_pie_graphic(data_keys, data_values){
+    var graphics_data = createDefaultData("pie", data_keys, data_values)
+
+    graphics_data.label = "Points"
+    graphics_data.context = document.getElementById("pie-chart-sphere").getContext('2d')
+    graphics_data.description = 'Porcentagem de agentes individuais e coletivos'
+
+    createChart(graphics_data)
+}
