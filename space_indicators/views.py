1c4ed7d09c3b82cc7dc3ef20c87d3d280b138694
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2017-11-12 00:18:41 -0200

    Congigure space indicators page.

--- space_indicators/views.py
+++ space_indicators/views.py
@@ -1,3 +1,5 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'space_indicators/space-indicators.html')
