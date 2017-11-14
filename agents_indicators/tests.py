0728d851fdf64c0f95ee98951d6bed4bd871138d
m7rodrigues <matheus.1nascimento58@gmail.com>
2017-11-14 00:20:19 -0200

    Create mock test requests agents raw data
    Signed-off-by: m7rodrigues <matheus.1nascimento58@gmail.com>
    Signed-off-by: Hugo Carvalho <hugo-nc@hotmail.com>

--- agents_indicators/tests.py
+++ agents_indicators/tests.py
@@ -4,6 +4,9 @@ from .models import PercentIndividualAndCollectiveAgent
 from .models import AmountAgentsRegisteredPerMonth
 from .models import PercentAgentsPerAreaOperation
 from .views import update_agent_indicator
+import requests_mock
+import requests
+import json
 
 
 class TestAmountAgentsRegisteredPerMonth(object):
@@ -11,7 +14,8 @@ class TestAmountAgentsRegisteredPerMonth(object):
     @staticmethod
     def total_agents_registered_month():
         AmountAgentsRegisteredPerMonth.drop_collection()
-        agents_in_month = AmountAgentsRegisteredPerMonth({"01": 10}, datetime.now().__str__())
+        agents_in_month = AmountAgentsRegisteredPerMonth(
+            {"01": 10}, datetime.now().__str__())
         agents_in_month.save()
         query = AmountAgentsRegisteredPerMonth.object.first()
         assert query.total_agents_registered_month == {"01": 10}
@@ -22,7 +26,8 @@ class TestPercentIndividualAndCollectiveAgent(object):
     @staticmethod
     def test_total_agents():
         PercentIndividualAndCollectiveAgent.drop_collection()
-        agent_indicator = PercentIndividualAndCollectiveAgent(50, datetime.now().__str__(), 10, 10)
+        agent_indicator = PercentIndividualAndCollectiveAgent(
+            50, datetime.now().__str__(), 10, 10)
         agent_indicator.save()
         query = PercentIndividualAndCollectiveAgent.objects.first()
         assert query.total_agents == 50
@@ -30,7 +35,8 @@ class TestPercentIndividualAndCollectiveAgent(object):
     @staticmethod
     def test_total_individual_agent():
         PercentIndividualAndCollectiveAgent.drop_collection()
-        agent_indicator = PercentIndividualAndCollectiveAgent(50, datetime.now().__str__(), 10, 10)
+        agent_indicator = PercentIndividualAndCollectiveAgent(
+            50, datetime.now().__str__(), 10, 10)
         agent_indicator.save()
         query = PercentIndividualAndCollectiveAgent.objects.first()
         assert query.total_individual_agent == 10
@@ -38,7 +44,8 @@ class TestPercentIndividualAndCollectiveAgent(object):
     @staticmethod
     def test_total_collective_agent():
         PercentIndividualAndCollectiveAgent.drop_collection()
-        agent_indicator = PercentIndividualAndCollectiveAgent(50, datetime.now().__str__(), 10, 10)
+        agent_indicator = PercentIndividualAndCollectiveAgent(
+            50, datetime.now().__str__(), 10, 10)
         agent_indicator.save()
         query = PercentIndividualAndCollectiveAgent.objects.first()
         assert query.total_collective_agent == 10
@@ -46,34 +53,22 @@ class TestPercentIndividualAndCollectiveAgent(object):
 
 class TestRequestAgentsRawData(object):
 
-    @staticmethod
-    def test_success_request():
+    @requests_mock.Mocker(kw='mock')
+    def test_success_request(self, **kwargs):
         current_time = datetime.now().__str__()
         url = "http://mapas.cultura.gov.br/api/"
+
+        result = {
+            'None': 1,
+        }
+
+        kwargs['mock'].get(url + "agent/find/", text=json.dumps(result))
+
         request_agents_raw_data = RequestAgentsRawData(current_time, url)
         response_agents_raw_data = request_agents_raw_data.response
         response_status_code = response_agents_raw_data.status_code
+
         assert response_status_code == 200
-
-    @staticmethod
-    def test_data_content():
-        current_time = datetime.now().__str__()
-        url = "http://mapas.cultura.gov.br/api/"
-        request_agents_raw_data = RequestAgentsRawData(current_time, url)
-        agents_raw_data = request_agents_raw_data.data
-        type_agents_raw_data = type(agents_raw_data)
-        empty_list = []
-        assert type_agents_raw_data == type(empty_list)
-
-    @staticmethod
-    def test_data_lenght():
-        current_time = datetime.now().__str__()
-        url = "http://mapas.cultura.gov.br/api/"
-        request_agents_raw_data = RequestAgentsRawData(current_time, url)
-        agents_raw_data = request_agents_raw_data.data_length
-        type_agents_raw_data = type(agents_raw_data)
-        intenger = 1
-        assert type_agents_raw_data == type(intenger)
 
 
 class TestPercentAgentsPerAreaOperation(object):
@@ -81,7 +76,8 @@ class TestPercentAgentsPerAreaOperation(object):
     @staticmethod
     def test_total_agents_area_oreration():
         PercentAgentsPerAreaOperation.drop_collection()
-        agent_indicator = PercentAgentsPerAreaOperation(50, datetime.now().__str__(), {"area": 10})
+        agent_indicator = PercentAgentsPerAreaOperation(
+            50, datetime.now().__str__(), {"area": 10})
         agent_indicator.save()
         query = PercentAgentsPerAreaOperation.objects.first()
         assert query.total_agents_area_oreration == {"area": 10}
