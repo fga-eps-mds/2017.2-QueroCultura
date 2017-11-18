4fbf008ac66253d34856403d9a67769b4cecab6a
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2017-11-17 23:36:47 -0200

    Create new models for project indicators.

--- project_indicators/models.py
+++ project_indicators/models.py
@@ -1,10 +1,9 @@
 from mongoengine import Document
-from mongoengine import IntField
 from mongoengine import StringField
-from mongoengine import DictField
+from mongoengine import DateTimeField
 
 
-class LastUpdateDate(Document):
+class LastUpdateProjectDate(Document):
     _create_date = StringField(required=True)
 
     @property
@@ -14,4 +13,42 @@ class LastUpdateDate(Document):
     @create_date.setter
     def create_date(self, create_date):
         self._create_date = create_date
- 
+
+
+class ProjectData(Document):
+    _instance = StringField(required=True)
+    _project_type = StringField(required=True)
+    _online_subscribe = StringField(required=True)
+    _date = DateTimeField(required=True)
+
+    @property
+    def instance(self):
+        return self._instance
+
+    @instance.setter
+    def instance(self, instance):
+        self._instance = instance
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, date):
+        self._date = date
+
+    @property
+    def online_subscribe(self):
+        return self._online_subscribe
+
+    @online_subscribe.setter
+    def create_date(self, online_subscribe):
+        self._online_subscribe = online_subscribe
+
+    @property
+    def project_type(self):
+        return self._project_type
+
+    @project_type.setter
+    def project_type(self, project_type):
+        self._project_type = project_type
