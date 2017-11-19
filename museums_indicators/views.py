513be6c5311edec56a8559153fd19368797d257b
Lucas Siqueira <lucassiqzro@gmail.com>
2017-11-19 18:00:03 -0200

    Remove old view from museum
    Signed-off-by: Lucas Siqueira <lucassiqzro@gmail.com>
    Signed-off-by: Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
    Signed-off-by: Matheus Rodrigues <matheus.1nascimento58@gmail.com>

--- museums_indicators/views.py
+++ museums_indicators/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from project_indicators.views import clean_url
 from .models import MuseumData
 from .models import OccupationArea
-from .models import LastUpdateDate
+from .models import LastUpdateMuseumDate
 from quero_cultura.views import ParserYAML
 import json
 
@@ -18,25 +18,10 @@ def index(request):
 
 
 def populate_museum_data():
-    if len(LastUpdateDate.objects) == 0:
-        LastUpdateDate(DEFAULT_INITIAL_DATE).save()
+    if len(LastUpdateMuseumDate.objects) == 0:
+        LastUpdateMuseumDate(DEFAULT_INITIAL_DATE).save()
 
-    size = LastUpdateDate.objects.count()
-    last_update = LastUpdateDate.objects[size - 1].create_date
+    size = LastUpdateMuseumDate.objects.count()
+    last_update = LastUpdateMuseumDate.objects[size - 1].create_date
 
-    parser_yaml = ParserYAML()
-    urls = parser_yaml.get_multi_instances_urls
-
-    for url in urls:
-        request = TestMuseum(last_update, url).data
-        new_url = clean_url(url)
-
-        for museum in request:
-            date = museum["createTimestamp"]['date']
-            accessibility = str(museum['acessibilidade'])
-            MuseumData(new_url, museum['name'], date,
-                      museum['type']['name'], accessibility).save()
-            for area in museum["terms"]["area"]:
-                OccupationArea(new_url, area).save()
-
-    LastUpdateDate(str(datetime.now())).save()
+    LastUpdateMuseumDate(str(datetime.now())).save()
