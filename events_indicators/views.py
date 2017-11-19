7a2defff82100e655c1fcc154abe308dcbaa80f3
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2017-11-19 16:11:16 -0200

    Add method to populate events data.
    Signed-off-by: Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
    Signed-off-by: Lucas Siqueira Rodrigues <lucassiqzro@gmail.com>

--- events_indicators/views.py
+++ events_indicators/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .api_connections import RequestEventsRawData
+from .models import EventData
+from .models import EventLanguage
+from .models import LastUpdateEventDate
+from project_indicators.views import clean_url
 from quero_cultura.views import ParserYAML
+from quero_cultura.views import get_metabase_url
 from datetime import datetime
 from celery.decorators import task
 
@@ -8,5 +13,34 @@ DEFAULT_INITIAL_DATE = "2012-01-01 15:47:38.337553"
 
 
 def index(request):
+    view_type = "question"
 
-    return render(request, 'events_indicators/events_indicators.html')
+    url = {"graphic1": get_metabase_url(view_type, 14),
+           "graphic2": get_metabase_url(view_type, 15),
+           "graphic3": get_metabase_url(view_type, 16),
+           "graphic4": get_metabase_url(view_type, 17)}
+
+    return render(request, 'events_indicators/events_indicators.html', url)
+
+
+@task(name="populate_event_data")
+def populate_event_data():
+    if len(LastUpdateEventDate.objects) == 0:
+        LastUpdateEventDate(DEFAULT_INITIAL_DATE).save()
+
+    size = LastUpdateEventDate.objects.count()
+    last_update = LastUpdateEventDate.objects[size - 1].create_date
+
+    parser_yaml = ParserYAML()
+    urls = parser_yaml.get_multi_instances_urls
+
+    for url in urls:
+        request = RequestEventsRawData(last_update, url).data
+        new_url = clean_url(url)
+        for event in request:
+            date = event["createTimestamp"]['date']
+            EventData(new_url, event['classificacaoEtaria'], date).save()
+            for language in event["terms"]["linguagem"]:
+                EventLanguage(new_url, language).save()
+
+    LastUpdateEventDate(str(datetime.now())).save()
