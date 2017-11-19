7437e56e8a1ea910d1c2ec5c557b8eb833747c70
Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
2017-11-19 19:52:11 -0200

    Fix pep8.
    Signed-off-by: Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
    Signed-off-by: Lucas Siqueira Rodrigues <lucassiqzro@gmail.com>

--- museums_indicators/api_connections.py
+++ museums_indicators/api_connections.py
@@ -7,10 +7,11 @@ class RequestMuseumRawData(object):
 
     def __init__(self, last_update_time, url):
         self._filters = {'@select': 'mus_tipo, mus_tipo_tematica, esfera, '
-                                   + 'mus_servicos_visitaGuiada, '
-                                   + 'mus_arquivo_acessoPublico, '
-                                   + 'createTimestamp',
-                        'createTimestamp': "GT("+last_update_time+")"}
+                                    + 'mus_servicos_visitaGuiada, '
+                                    + 'mus_arquivo_acessoPublico, '
+                                    + 'createTimestamp',
+                                    'createTimestamp':
+                                    "GT("+last_update_time+")"}
         self._response = requests.get(url, self._filters)
         self._data = json.loads(self._response.text)
 
