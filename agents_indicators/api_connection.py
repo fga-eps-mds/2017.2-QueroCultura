a16143ef0d5690b3e354a88f0bed115afbf92b9c
pablodiegoss <pablodiegoss@hotmail.com>
2017-11-19 14:18:55 -0200

    Fix SP agents query to API

--- agents_indicators/api_connection.py
+++ agents_indicators/api_connection.py
@@ -1,5 +1,46 @@
 import json
+import datetime
 import requests
+
+
+class RequestAgentsInPeriod(object):
+
+    def __init__(self, year, url):
+        self._filters = ''
+        self._data = []
+        for i in range(1, 13):
+            if i < 12:
+                initial_date = datetime.date(year, i, 1)
+                final_date = datetime.date(year, i+1, 1)
+            else:
+                initial_date = datetime.date(year, i, 1)
+                final_date = datetime.date(year+1, 1, 1)
+
+            print("\nINITIALDATE = " + str(initial_date))
+            print("\nFINALDATE = " + str(final_date))
+            data = self.request_data(initial_date, final_date, url)
+
+            self._data += json.loads(data.text)
+
+    def request_data(self, initial_date, final_date, url):
+        self._filters = {'@select': 'terms,type, createTimestamp, updateTimestamp',
+                         'createTimestamp': "BET("+str(initial_date)+","+str(final_date)+")",
+                        }
+        self._response = requests.get(url + "agent/find/", self._filters)
+        print(self._response.url)
+        return self._response
+
+    @property
+    def response(self):
+        return self._response
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def data_length(self):
+        return len(self._data)
 
 
 class RequestAgentsRawData(object):
@@ -17,6 +58,24 @@ class RequestAgentsRawData(object):
     @property
     def data(self):
         return self._data
+
+    @property
+    def data_length(self):
+        return len(self._data)
+
+
+class EmptyRequest(object):
+
+    def __init__(self):
+        self._data = []
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, data):
+        self._data = data
 
     @property
     def data_length(self):
--- agents_indicators/views.py
+++ agents_indicators/views.py
@@ -1,4 +1,6 @@
 from .api_connection import RequestAgentsRawData
+from .api_connection import RequestAgentsInPeriod
+from .api_connection import EmptyRequest
 from django.shortcuts import render
 from .models import PercentIndividualAndCollectiveAgent
 from .models import AmountAgentsRegisteredPerMonth
@@ -93,7 +95,7 @@ def build_type_indicator(new_data):
 
 @task(name="update_agent_indicator")
 def update_agent_indicator():
-    url = "http://mapas.cultura.gov.br/api/"
+    url = "http://spcultura.prefeitura.sp.gov.br/api/"
 
     # Cria registro inicial caso seja o primeiro uso da aplicação
     if len(PercentIndividualAndCollectiveAgent.objects) == 0:
@@ -110,7 +112,19 @@ def update_agent_indicator():
     last_temporal = AmountAgentsRegisteredPerMonth.objects[index-1]
 
     # Requisita dados da API MinC a partir de data passada por parmetro
-    request = RequestAgentsRawData(last_per_area.create_date, url)
+    if(last_per_area.create_date == DEFAULT_INITIAL_DATE):
+        this_year = datetime.today().year
+
+        # DEFAULT_INITIAL_DATE.year
+        default_year = 2012
+        request = EmptyRequest()
+        for year in range(default_year, this_year+1):
+            single_request = RequestAgentsInPeriod(year, url)
+            request.data += single_request.data
+
+        print(request.data_length)
+    else:
+        request = RequestAgentsRawData(last_per_area.create_date, url)
 
     # Geração indicadores de agentes atualizados
     new_total = request.data_length + last_per_area.total_agents
