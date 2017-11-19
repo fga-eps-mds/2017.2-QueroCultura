4a8890df7baa8ec99daa9c95bc676ae4c1f4d3a4
Lucas Siqueira <lucassiqzro@gmail.com>
2017-11-19 17:57:31 -0200

    Refactor models from museums
    Signed-off-by: Lucas Siqueira <lucassiqzro@gmail.com>
    Signed-off-by: Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
    Signed-off-by: Matheus Rodrigues <matheus.1nascimento58@gmail.com>

--- museums_indicators/models.py
+++ museums_indicators/models.py
@@ -3,10 +3,8 @@ from mongoengine import IntField
 from mongoengine import StringField
 from mongoengine import DateTimeField
 
-# -------------------- musuem indicators --------------------------------
 
-
-class LastUpdateDate(Document):
+class LastUpdateMuseumDate(Document):
     _create_date = StringField(required=True)
 
     @property
@@ -18,35 +16,15 @@ class LastUpdateDate(Document):
         self._create_date = create_date
 
 
-class OccupationArea(Document):
-    _instance = StringField(required=True)
-    _occupation_area = StringField(required=True)
-
-    @property
-    def instance(self):
-        return self._instance
-
-    @instance.setter
-    def instance(self, instance):
-        self._instance = instance
-
-    @property
-    def occupation_area(self):
-        return self._occupation_area
-
-    @occupation_area.setter
-    def occupation_area(self, occupation_area):
-        self._occupation_area = occupation_area
-
 class MuseumData(Document):
-    abstract = True
-    meta = {'allow_inheritance': True}
 
     _instance = StringField(required=True)
-    _name = StringField(required=True)
-    _date = DateTimeField(required=True)
     _museum_type = StringField(required=True)
-    _accessibility = StringField(required=True)
+    _tematic = StringField(required=True)
+    _sphere = StringField(required=True)
+    _guided_tuor = StringField(required=True)
+    _plubic_archive = StringField(required=True)
+    _date = DateTimeField(required=True)
 
     @property
     def instance(self):
@@ -55,22 +33,6 @@ class MuseumData(Document):
     @instance.setter
     def instance(self, instance):
         self._instance = instance
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, name):
-        self._name = name
-
-    @property
-    def date(self):
-        return self._date
-
-    @date.setter
-    def date(self, date):
-        self._date = date
 
     @property
     def museum_type(self):
@@ -81,9 +43,41 @@ class MuseumData(Document):
         self._museum_type = museum_type
 
     @property
-    def accessibility(self):
-        return self._accessibility
+    def date(self):
+        return self._date
 
-    @accessibility.setter
-    def accessibility(self, accessibility):
-        self._accessibility = accessibility
+    @date.setter
+    def date(self, date):
+        self._date = date
+
+    @property
+    def tematic(self):
+        return self._tematic
+
+    @tematic.setter
+    def tematic(self,tematic):
+        self._tematic = tematic
+
+    @property
+    def sphere(self):
+        return self._sphere
+
+    @sphere.setter
+    def sphere(self, sphere):
+        self._sphere = sphere
+
+    @property
+    def guided_tuor(self):
+        return self._guided_tuor
+
+    @guided_tuor.setter
+    def guided_tuor(self, guided_tuor):
+        self._guided_tuor = guided_tuor
+
+    @property
+    def public_archive(self):
+        return self._public_archive
+
+    @public_archive.setter
+    def public_archive(self, public_archive):
+        self._public_archive = public_archive
