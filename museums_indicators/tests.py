67d895423f2c30de7bc155a6fadb245bf1f6bb38
Lucas Siqueira <lucassiqzro@gmail.com>
2017-11-20 22:24:15 -0200

    Create new tests for museum indicators
    Signed-off-by: Lucas Siqueira <lucassiqzro@gmail.com>
    Signed-off-by: Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
    Signed-off-by: Matheus Rodrigues <matheus.1nascimento58@gmail.com>

--- museums_indicators/tests.py
+++ museums_indicators/tests.py
@@ -3,6 +3,7 @@ from .api_connections import RequestMuseumRawData
 from .models import LastUpdateMuseumDate
 from .models import MuseumData
 from .models import LastUpdateMuseumDate
+from .views import populate_museum_data
 import requests_mock
 import json
 
@@ -60,3 +61,25 @@ class TestRequestMuseumRawData(object):
         assert raw_data.response.status_code == 200
         assert raw_data.data == result
         assert raw_data.data_length == 1
+
+
+class TestPopulateMuseumData(object):
+    @requests_mock.Mocker(kw='mock')
+    def test_populate_museum_data(self, **kwargs):
+        url = "http://museus.cultura.gov.br/api/"
+
+        result = [{"createTimestamp": {"date": "2012-01-01 00:00:00.000000"},
+                   "esfera": "Publica", "mus_tipo": 'None',
+                   "mus_tipo_tematica": 'None',
+                   "mus_servicos_visitaGuiada": 'None',
+                   "mus_arquivo_acessoPublico": 'None'}]
+
+        kwargs['mock'].get(url+"space/find/", text=json.dumps(result))
+
+        LastUpdateMuseumDate.drop_collection()
+        MuseumData.drop_collection()
+
+        populate_museum_data()
+
+        assert LastUpdateMuseumDate.objects.count() != 0
+        assert MuseumData.objects.count() != 0
