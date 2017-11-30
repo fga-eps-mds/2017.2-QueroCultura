e43543b5ebe5fde5385e337a07ca55b501811886
cjjcastro <cjj.castro3@gmail.com>
2017-11-30 21:07:12 -0200

    Update models in mixed indicators

--- mixed_indicators/models.py
+++ mixed_indicators/models.py
@@ -1,3 +1,16 @@
-# from mongoengine import Document
-# from mongoengine import StringField
-# from mongoengine import DateTimeField
+from mongoengine import Document
+from mongoengine import StringField
+from mongoengine import DateTimeField
+
+class LastUpdateMixedDate(Document):
+    _create_date = StringField(required=True)
+
+    @property
+    def create_date(self):
+        return self._create_date
+
+    @create_date.setter
+    def create_date(self, create_date):
+        self._create_date = create_date
+
+        
