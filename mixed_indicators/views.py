9995d4951878ffc5eac32b3168d3a2d363ff1852
cjjcastro <cjj.castro3@gmail.com>
2017-11-30 21:24:20 -0200

    Configure mixed indicator page

--- mixed_indicators/views.py
+++ mixed_indicators/views.py
@@ -1,3 +1,6 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+
+    return render(request, 'mixed_indicators/mixed-indicators.html')
