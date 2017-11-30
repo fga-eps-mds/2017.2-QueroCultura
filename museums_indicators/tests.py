3138047bac315cfa9387ac68c7d2ec35ce6e056f
Hugo Carvalho <hugo-nc@hotmail.com>
2017-11-29 23:16:46 -0200

    Fix tests for museum and library models

--- museums_indicators/tests.py
+++ museums_indicators/tests.py
@@ -2,14 +2,16 @@ from datetime import datetime
 from .api_connections import RequestMuseumRawData
 from .models import LastUpdateMuseumDate
 from .models import MuseumData
+from .models import MuseumArea
+from .models import MuseumTags
 from .models import LastUpdateMuseumDate
 from .views import populate_museum_data
+from quero_cultura.views import ParserYAML
 import requests_mock
 import json
 
 
 class TestLastUpdateMuseumDate(object):
-
     def test_last_update_museum_date(self):
         LastUpdateMuseumDate.drop_collection()
         create_date = datetime.now().__str__()
@@ -18,40 +20,53 @@ class TestLastUpdateMuseumDate(object):
         assert query.create_date == create_date
 
 
-class TestMuseumData(object):
+class TestMuseumArea(object):
+    def test_museum_area(self):
+        MuseumArea.drop_collection()
+        instance = "SP"
+        area = "Cinema"
+        MuseumArea(instance, area).save()
+        query = MuseumArea.objects.first()
+        assert query.instance == instance
+        assert query.area == area
 
+
+class TestMuseumTags(object):
+    def test_museum_area(self):
+        MuseumTags.drop_collection()
+        instance = "SP"
+        tag = "OlavoBilac"
+        MuseumTags(instance, tag).save()
+        query = MuseumTags.objects.first()
+        assert query.instance == instance
+        assert query.tag == tag
+
+
+class TestMuseumData(object):
     def test_museum_data(self):
         MuseumData.drop_collection()
         instance = "SP"
         museum_type = "Cia"
+        accessibility = "Sim"
         date = datetime(2017, 11, 14, 3, 5, 55, 88000)
-        thematic = "Teatro"
-        sphere = "None"
-        guided_tuor = "None"
-        public_archive = "None"
 
-        MuseumData(instance, museum_type, thematic, sphere,
-                   guided_tuor, public_archive, date).save()
+        MuseumData(instance, museum_type, accessibility, date).save()
         query = MuseumData.objects.first()
         assert query.instance == instance
         assert query.museum_type == museum_type
-        assert query.thematic == thematic
-        assert query.sphere == sphere
-        assert query.guided_tuor == guided_tuor
-        assert query.public_archive == public_archive
+        assert query.accessibility == accessibility
         assert query.date == date
 
 
 class TestRequestMuseumRawData(object):
     @requests_mock.Mocker(kw='mock')
     def test_request_museum_raw_data(self, **kwargs):
-        url = "http://museus.cultura.gov.br/api/"
+        url = "http://mapas.cultura.gov.br/api/"
 
         result = [{"createTimestamp": {"date": "2012-01-01 00:00:00.000000"},
-                   "esfera": "Publica", "mus_tipo": 'None',
-                   "mus_tipo_tematica": 'None',
-                   "mus_servicos_visitaGuiada": 'None',
-                   "mus_arquivo_acessoPublico": 'None'}]
+                   "acessibilidade": "Sim",
+                   "type": {"id":"60", "name":"Museum Publico"},
+                   "terms": {"area": ["Cinema", "Teatro"], "tag":["Olavo Bilac"]}}]
 
         kwargs['mock'].get(url+"space/find/", text=json.dumps(result))
 
@@ -65,18 +80,21 @@ class TestRequestMuseumRawData(object):
 class TestPopulateMuseumData(object):
     @requests_mock.Mocker(kw='mock')
     def test_populate_museum_data(self, **kwargs):
-        url = "http://museus.cultura.gov.br/api/"
+        parser_yaml = ParserYAML()
+        urls = parser_yaml.get_multi_instances_urls
 
         result = [{"createTimestamp": {"date": "2012-01-01 00:00:00.000000"},
-                   "esfera": "Publica", "mus_tipo": 'None',
-                   "mus_tipo_tematica": 'None',
-                   "mus_servicos_visitaGuiada": 'None',
-                   "mus_arquivo_acessoPublico": 'None'}]
+                   "acessibilidade": "Sim",
+                   "type": {"id":"60", "name":"Museum Publico"},
+                   "terms": {"area": ["Cinema", "Teatro"], "tag":["Olavo Bilac"]}}]
 
-        kwargs['mock'].get(url+"space/find/", text=json.dumps(result))
+        for url in urls:
+            kwargs['mock'].get(url + "space/find/", text=json.dumps(result))
 
         LastUpdateMuseumDate.drop_collection()
         MuseumData.drop_collection()
+        MuseumArea.drop_collection()
+        MuseumTags.drop_collection()
 
         populate_museum_data()
 
