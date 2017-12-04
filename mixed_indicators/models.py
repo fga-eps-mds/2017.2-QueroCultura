c805fcf628bcd4955db7d418e33d9915f93dac01
Lucas Siqueira <lucassiqzro@gmail.com>
2017-12-04 21:51:45 -0200

    Create models from mixed indicators
    Signed-off-by: Lucas Siqueira <lucassiqzro@gmail.com>
    Signed-off-by: cjjcastro <cjj.castro3@gmail.com>
    Signed-off-by: Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
    Signed-off-by: Matheus Rodrigues <matheus.1nascimento58@gmail.com>

--- mixed_indicators/models.py
+++ mixed_indicators/models.py
@@ -13,4 +13,41 @@ class LastUpdateMixedDate(Document):
     def create_date(self, create_date):
         self._create_date = create_date
 
-        
+
+class EventAndSpaceData(Document):
+    _instance = StringField(required=True)
+    _name = StringField(required=True)
+    _accessible_space = StringField(required=True)
+    _date = DateTimeField(required=True)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(sel, name):
+        self._name = name
+
+    @property
+    def accessible_space(self):
+        return self._accessible_space
+
+    @accessible_space.setter
+    def accessible_space(self, accessible_space):
+        self._accessible_space = accessible_space
+
+    @property
+    def instance(self):
+        return self._instance
+
+    @instance.setter
+    def instance(self, instance):
+        self._instance = instance
+
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, date):
+        self._date = date
