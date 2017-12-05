ad780eec6456a407b09a899e16023b763e7d3c96
m7rodrigues <matheus.1nascimento58@gmail.com>
2017-12-04 23:41:16 -0200

    Create test requesst mixed raw data
    Signed-off-by: Matheus rodrigues <matheus.1nascimento58@gmail.com>
    Signed-off-by: Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
    Signed-off-by: Lucas Siqueira Rodrigues <lucassiqzro@gmail.com>
    Signed-off-by: cjjcastro <cjj.castro3@gmail.com>

--- mixed_indicators/tests.py
+++ mixed_indicators/tests.py
@@ -56,3 +56,22 @@ class TestPopulateMixedData(object):
 
         assert LastUpdateMixedDate.objects.count() != 0
         assert EventAndSpaceData.objects.count() != 0
+
+
+class TestRequestMixedRawData(object):
+
+    @requests_mock.Mocker(kw='mock')
+    def test_request_mixed_raw_data(self, **kwargs):
+        url = "http://mapas.cultura.gov.br/api/"
+
+        result = [{'occurrences': [
+            {'space': {'acessibilidade': 'Sim', 'id': 13242}, 'id': 401}], 'id': 882}]
+
+        kwargs['mock'].get(url + "event/find/", text=json.dumps(result))
+
+        current_time = datetime.now().__str__()
+        raw_data = RequestMixedIndicatorsRawData(current_time,
+                                                 "http://mapas.cultura.gov.br/api/")
+        assert raw_data.response.status_code == 200
+        assert raw_data.data == result
+        assert raw_data.data_length == 1
