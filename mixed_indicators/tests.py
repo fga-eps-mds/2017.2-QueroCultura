a9176ad4e9ff54f08621c45caf5d593561f95eb0
m7rodrigues <matheus.1nascimento58@gmail.com>
2017-12-04 23:33:21 -0200

    Create test EventAndSpaceData in mixed indicators
    Signed-off-by: m7rodrigues <matheus.1nascimento58@gmail.com>

--- mixed_indicators/tests.py
+++ mixed_indicators/tests.py
@@ -16,3 +16,19 @@ class TestLastUpdateMixedDate(object):
         LastUpdateMixedDate(create_date).save()
         query = LastUpdateMixedDate.objects.first()
         assert query.create_date == create_date
+
+
+class TestSpaceData(object):
+
+    def test_event_space_data(self):
+        EventAndSpaceData.drop_collection()
+        instance = "SP"
+        name = "Cia"
+        accessible_space = "sim"
+        date = datetime(2017, 11, 14, 3, 5, 55, 88000)
+        EventAndSpaceData(instance, name, accessible_space, date).save()
+        query = EventAndSpaceData.objects.first()
+        assert query.instance == instance
+        assert query.name == name
+        assert query.accessible_space == accessible_space
+        assert query.date == date
