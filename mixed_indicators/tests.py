a17fd3a366ee377bf9eba23dcab88bc1571d2be5
m7rodrigues <matheus.1nascimento58@gmail.com>
2017-12-04 23:27:03 -0200

    Create test last update mixed indicators
    Signed-off-by: m7rodrigues <matheus.1nascimento58@gmail.com>
    Signed-off-by: cjjcastro <cjj.castro3@gmail.com>
    Signed-off-by: Lucas Pereira de Andrade Macêdo <lpalucas.10@gmail.com>
    Signed-off-by: Lucas Siqueira Rodrigues <lucassiqzro@gmail.com>

--- mixed_indicators/tests.py
+++ mixed_indicators/tests.py
@@ -1,21 +1,18 @@
 from datetime import datetime
 from .api_connections import RequestMixedIndicatorsRawData
+from .models import LastUpdateMixedDate
+from .models import EventAndSpaceData
+from .views import populate_mixed_data
+from quero_cultura.views import ParserYAML
 import requests_mock
 import json
 
 
-class TestClassRequestMixedRawData(object):
-    @requests_mock.Mocker(kw='mock')
-    def test_request_mixed_raw_data(self, **kwargs):
-        url = "http://mapas.cultura.gov.br/api/"
+class TestLastUpdateMixedDate(object):
 
-        result = [{'occurrences': [{'space': {'acessibilidade': 'Sim', 'id': 13242}, 'id': 401}], 'id': 882}]
-
-        kwargs['mock'].get(url + "event/find/", text=json.dumps(result))
-
-        current_time = datetime.now().__str__()
-        raw_data = RequestMixedIndicatorsRawData(current_time,
-                                          "http://mapas.cultura.gov.br/api/")
-        assert raw_data.response.status_code == 200
-        assert raw_data.data == result
-        assert raw_data.data_length == 1
+    def test_last_update_mixed_date(self):
+        LastUpdateMixedDate.drop_collection()
+        create_date = datetime.now().__str__()
+        LastUpdateMixedDate(create_date).save()
+        query = LastUpdateMixedDate.objects.first()
+        assert query.create_date == create_date
