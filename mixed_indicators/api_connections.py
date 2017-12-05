7d5dd6b1ef477829b284e9814ddfcf7311d610e9
Lucas Siqueira <lucassiqzro@gmail.com>
2017-12-05 02:14:07 -0200

    Update api connection from mixed indicators
    Signed-off-by: Lucas Siqueira <lucassiqzro@gmail.com>
    Signed-off-by: Matheus Rodrigues <matheus.1nascimento58@gmail.com>

--- mixed_indicators/api_connections.py
+++ mixed_indicators/api_connections.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 import requests
 
 
@@ -17,6 +18,61 @@ class RequestMixedIndicatorsRawData(object):
     @property
     def data(self):
         return self._data
+
+    @property
+    def data_length(self):
+        return len(self._data)
+
+
+class RequestMixedInPeriod(object):
+
+    def __init__(self, year, url):
+        self._filters = ''
+        self._data = []
+        for i in range(1, 13):
+            if i < 12:
+                initial_date = datetime.date(year, i, 1)
+                final_date = datetime.date(year, i+1, 1)
+            else:
+                initial_date = datetime.date(year, i, 1)
+                final_date = datetime.date(year+1, 1, 1)
+
+            data = self.request_data(initial_date, final_date, url)
+
+            self._data += json.loads(data.text)
+
+    def request_data(self, initial_date, final_date, url):
+        self._filters = {'@select': 'occurrences.{space.{acessibilidade}}, name, createTimestamp',
+                         'createTimestamp': "BET(" + str(initial_date) + ","
+                         + str(final_date) + ")"}
+        self._response = requests.get(url+"event/find/", self._filters)
+        self._data = json.loads(self._response.text)
+
+    @property
+    def response(self):
+        return self._response
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def data_length(self):
+        return len(self._data)
+
+
+class EmptyRequest(object):
+
+    def __init__(self):
+        self._data = []
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, data):
+        self._data = data
 
     @property
     def data_length(self):
